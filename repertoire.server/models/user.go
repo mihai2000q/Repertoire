9fbb1e353fe796a65ab4d47771d343f377370416
Cris <filipcristimihail@yahoo.com>
2024-10-08 16:58:02 +0200

    #3 Server - User - Add Not Null to GORM Constraints

--- repertoire.server/models/user.go
+++ repertoire.server/models/user.go
@@ -7,9 +7,9 @@ import (
 
 type User struct {
 	ID        uuid.UUID `gorm:"type:uuid; <-:create" json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `gorm:"unique" json:"email"`
-	Password  string    `json:"-"`
-	CreatedAt time.Time `gorm:"<-:create" json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `gorm:"not null" json:"name"`
+	Email     string    `gorm:"unique; not null" json:"email"`
+	Password  string    `gorm:"not null" json:"-"`
+	CreatedAt time.Time `gorm:"not null; <-:create" json:"created_at"`
+	UpdatedAt time.Time `gorm:"not null" json:"updated_at"`
 }
