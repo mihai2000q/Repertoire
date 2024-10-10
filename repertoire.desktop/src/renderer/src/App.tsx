96985cecfe76eac986b0fedc89f80961cc554d1d
Cris <filipcristimihail@yahoo.com>
2024-10-10 16:46:11 +0200

    #11 Desktop - populate the App and add The Router and redirection for the workflow of the application

--- repertoire.desktop/src/renderer/src/App.tsx
+++ repertoire.desktop/src/renderer/src/App.tsx
@@ -1,5 +1,37 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import IsAlreadyAuthenticated from '@renderer/router/IsAlreadyAuthenticated'
+import SignUpView from '@renderer/views/SignUpView'
+import RequireAuthentication from '@renderer/router/RequireAuthentication'
+import HomeView from '@renderer/views/HomeView'
+import NotFoundView from '@renderer/views/NotFoundView'
+import UnauthorizedView from '@renderer/views/UnauthorizedView'
+import { ReactElement } from 'react'
+import MainView from '@renderer/views/MainView'
 
+function App(): ReactElement {
   return (
+    <div className={'app'}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Navigate to={'home'} replace />} />
+
+          <Route element={<IsAlreadyAuthenticated />}>
+            <Route path={'sign-up'} element={<SignUpView />} />
+          </Route>
+
+          <Route element={<RequireAuthentication />}>
+            <Route element={<MainView />}>
+              <Route path={'home'} element={<HomeView />} />
+
+              {/* Errors */}
+              <Route path={'401'} element={<UnauthorizedView />} />
+              <Route path={'404'} element={<NotFoundView />} />
+              <Route path={'*'} element={<Navigate to={'404'} replace />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
