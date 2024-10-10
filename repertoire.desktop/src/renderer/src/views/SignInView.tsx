4006a3bc4d7b6a918883db21e778a51c82360492
Cris <filipcristimihail@yahoo.com>
2024-10-10 16:44:45 +0200

    #11 Desktop - Add Standard Empty Views

---
+++ repertoire.desktop/src/renderer/src/views/SignInView.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react'
+
+function SignInView(): ReactElement {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <div>Right here</div>
+    </div>
+  )
+}
+
+export default SignInView
---
+++ repertoire.desktop/src/renderer/src/views/UnauthorizedView.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react'
+
+function UnauthorizedView(): ReactElement {
+  return (
+    <div>
+      <h1>Unauthorized</h1>
+      <p>You shouldn&#39;t be here</p>
+    </div>
+  )
+}
+
+export default UnauthorizedView
