f039bffa00fb5d12fce42e7b68989d1e01e24a0a
Cris <filipcristimihail@yahoo.com>
2024-10-11 23:05:43 +0200

    #17 Desktop - Add Custom Port to Vite Configuration

--- repertoire.desktop/electron.vite.config.ts
+++ repertoire.desktop/electron.vite.config.ts
@@ -1,20 +1,28 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return {
+    main: {
+      plugins: [externalizeDepsPlugin()]
     },
-    plugins: [react()]
+    preload: {
+      plugins: [externalizeDepsPlugin()]
+    },
+    renderer: {
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src')
+        }
+      },
+      plugins: [react()],
+      server: {
+        port: parseInt(env.VITE_APPLICATION_PORT)
+      }
+    }
   }
 })
