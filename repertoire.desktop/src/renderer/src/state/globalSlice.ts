01e6437b5cda51ff9b840490d7d939abb50adfaf
Cris <filipcristimihail@yahoo.com>
2024-10-11 12:32:31 +0200

    #15 Desktop - Global Slice - Remove the setMode reducer (the app is intended to have only one mode, and mantine takes care of that anyways)

--- repertoire.desktop/src/renderer/src/state/globalSlice.ts
+++ repertoire.desktop/src/renderer/src/state/globalSlice.ts
@@ -1,14 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { ThemeMode } from '@renderer/theme/theme'
 
 export interface GlobalState {
-  mode: ThemeMode
   userId: string | undefined
   errorPath: string | undefined
 }
 
 const initialState: GlobalState = {
-  mode: 'dark',
   userId: undefined,
   errorPath: undefined
 }
@@ -17,9 +14,6 @@ export const globalSlice = createSlice({
   name: 'global',
   initialState,
   reducers: {
-    setMode: (state) => {
-      state.mode = state.mode === 'light' ? 'dark' : 'light'
-    },
     setUserId: (state, action: PayloadAction<string | undefined>) => {
       state.userId = action.payload
     },
@@ -29,6 +23,6 @@ export const globalSlice = createSlice({
   }
 })
 
-export const { setMode, setUserId, setErrorPath } = globalSlice.actions
+export const { setUserId, setErrorPath } = globalSlice.actions
 
 export default globalSlice.reducer
