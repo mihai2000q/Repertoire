ab39d687e6e0673ba127fba4686317533737ee6d
Cris <FilipCristiMihail@Yahoo.com>
2024-10-13 17:05:39 +0200

    #18 Server - Api - Validation - Add New Non Standard Validator: notblank

--- repertoire.server/api/validation/validator.go
+++ repertoire.server/api/validation/validator.go
@@ -1,8 +1,10 @@
 package validation
 
 import (
-	"github.com/go-playground/validator/v10"
 	"repertoire/utils"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator/v10/non-standard/validators"
 )
 
 type Validator struct {
@@ -10,8 +12,10 @@ type Validator struct {
 }
 
 func NewValidator() Validator {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	registerCustomValidators(validate)
 	return Validator{
-		validate: validator.New(validator.WithRequiredStructEnabled()),
+		validate: validate,
 	}
 }
 
@@ -21,4 +25,8 @@ func (v *Validator) Validate(request interface{}) *utils.ErrorCode {
 		return utils.BadRequestError(err)
 	}
 	return nil
+}
+
+func registerCustomValidators(validate *validator.Validate) {
+	validate.RegisterValidation("notblank", validators.NotBlank)
 }
