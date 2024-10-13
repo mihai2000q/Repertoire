d98cec8351732a3952b127e2a31bd3cbfeb1d8d4
Cris <FilipCristiMihail@Yahoo.com>
2024-10-13 19:28:53 +0200

    wip

--- repertoire.server/models/song.go
+++ repertoire.server/models/song.go
@@ -10,6 +10,7 @@ type Song struct {
 	ID         uuid.UUID `gorm:"type:uuid; <-:create" json:"id"`
 	Title      string    `gorm:"not null" json:"title"`
 	IsRecorded *bool     `json:"isRecorded"`
+	User       User      `gorm:"not null" json:"-"`
 	CreatedAt  time.Time `gorm:"not null; <-:create" json:"created_at"`
 	UpdatedAt  time.Time `gorm:"not null" json:"updated_at"`
 }
