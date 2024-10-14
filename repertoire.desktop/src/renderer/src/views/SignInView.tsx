3e3a05c92a14fa446f822b81a06870577ba9da22
Cris <filipcristimihail@yahoo.com>
2024-10-14 16:04:02 +0200

    #17 Desktop - Refactor Sign In View

--- repertoire.desktop/src/renderer/src/views/SignInView.tsx
+++ repertoire.desktop/src/renderer/src/views/SignInView.tsx
@@ -15,26 +15,27 @@ import { useSignInMutation } from '@renderer/state/api'
 import { useAppDispatch } from '@renderer/state/store'
 import { setToken } from '@renderer/state/authSlice'
 import HttpErrorResponse from '@renderer/types/HttpError.response'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function SignInView(): ReactElement {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const [signIn, { error, isLoading }] = useSignInMutation()
-  const loginError = (error as HttpErrorResponse | undefined)?.data
+  const [signInMutation, { error, isLoading }] = useSignInMutation()
+  const loginError = (error as HttpErrorResponse | undefined)?.data?.error
 
-  async function onSubmit(): Promise<void> {
+  async function signIn(): Promise<void> {
     try {
-      const res = await signIn({
-        email: email,
-        password: password
-      }).unwrap()
+      const res = await signInMutation({ email, password }).unwrap()
 
       dispatch(setToken(res.token))
+      navigate(location.state?.from?.pathname ?? 'home')
     } catch (e) {
-      window.electron.ipcRenderer.send('log', e)
+      /*ignored*/
     }
   }
 
@@ -51,35 +52,33 @@ function SignInView(): ReactElement {
           </Anchor>
         </Text>
 
-        <Paper withBorder shadow="xl" p={30} mt={15}>
-          <form onSubmit={onSubmit}>
-            <Stack align={'flex-start'} gap={0} w={200}>
-              <Stack w={'100%'}>
-                <TextInput
-                  required
-                  label="Email"
-                  placeholder="Your email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  error={loginError?.title}
-                />
-                <PasswordInput
-                  required
-                  label="Password"
-                  placeholder="Your password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  error={loginError?.title}
-                />
-              </Stack>
-              <Anchor component="button" size="sm" mt={6} style={{ alignSelf: 'flex-end' }}>
-                Forgot password?
-              </Anchor>
-              <Button fullWidth mt={'sm'} type={'submit'} disabled={isLoading}>
-                Sign in
-              </Button>
+        <Paper withBorder shadow="md" p={30} mt={15}>
+          <Stack align={'flex-start'} gap={0} w={200}>
+            <Stack w={'100%'}>
+              <TextInput
+                required
+                label="Email"
+                placeholder="Your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={loginError}
+              />
+              <PasswordInput
+                required
+                label="Password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                error={loginError}
+              />
             </Stack>
-          </form>
+            <Anchor component="button" size="sm" mt={6} style={{ alignSelf: 'flex-end' }}>
+              Forgot password?
+            </Anchor>
+            <Button fullWidth mt={'sm'} disabled={isLoading} onClick={signIn}>
+              Sign in
+            </Button>
+          </Stack>
         </Paper>
       </Flex>
     </Container>
