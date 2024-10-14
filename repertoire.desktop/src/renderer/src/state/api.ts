3f67585a032a14038c83683ff9d84fc165212d75
Cris <filipcristimihail@yahoo.com>
2024-10-14 10:05:17 +0200

    #17 Desktop - Api - Add Auth Sign In PUT Endpoint

--- repertoire.desktop/src/renderer/src/state/api.ts
+++ repertoire.desktop/src/renderer/src/state/api.ts
@@ -9,10 +9,20 @@ export const api = createApi({
   reducerPath: 'api',
   tagTypes: [],
   endpoints: (build) => ({
-    getCurrentUser: build.query({
+    // Auth
+    signIn: build.mutation<TokenResponse, SignInRequest>({
+      query: (body) => ({
+        url: `auth/sign-in`,
+        method: 'PUT',
+        body: body
+      })
+    }),
+
+    // Users
+    getCurrentUser: build.query<User, undefined>({
       query: () => `users/current`
     })
   })
 })
 
-export const { useGetCurrentUserQuery } = api
+export const { useSignInMutation, useGetCurrentUserQuery } = api
