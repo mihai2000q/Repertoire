db4ff158ef84dd50a2f3a615bc14d6581cb65212
Cris <FilipCristiMihail@Yahoo.com>
2024-10-15 21:00:02 +0200

    #44 Server - New Model: Playlist with many 2 many relationship with Song

---
+++ repertoire.server/models/playlist.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Playlist struct {
+	ID          uuid.UUID `gorm:"primaryKey; type:uuid; <-:create" json:"id"`
+	Title       string    `gorm:"size:100; not null" json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `gorm:"default:current_timestamp; not null; <-:create" json:"createdAt"`
+	UpdatedAt   time.Time `gorm:"default:current_timestamp; not null" json:"updatedAt"`
+	UserID      uuid.UUID `gorm:"not null" json:"-"`
+	Songs       []Song    `gorm:"many2many:playlist_song" json:"-"`
+}
