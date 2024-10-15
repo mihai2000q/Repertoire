c66ab2621c127cfd52d58c1cc8ca566988bb35d7
Cris <FilipCristiMihail@Yahoo.com>
2024-10-15 21:44:48 +0200

    #44 Server - Domain: New Use Cases for Playlist

--- repertoire.server/models/playlist.go
+++ repertoire.server/models/playlist.go
@@ -9,7 +9,7 @@ import (
 type Playlist struct {
 	ID          uuid.UUID `gorm:"primaryKey; type:uuid; <-:create" json:"id"`
 	Title       string    `gorm:"size:100; not null" json:"title"`
-	Description string    `json:"description"`
+	Description string    `gorm:"not null" json:"description"`
 	CreatedAt   time.Time `gorm:"default:current_timestamp; not null; <-:create" json:"createdAt"`
 	UpdatedAt   time.Time `gorm:"default:current_timestamp; not null" json:"updatedAt"`
 	UserID      uuid.UUID `gorm:"not null" json:"-"`
