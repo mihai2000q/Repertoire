945e59f2d2a6af5136bb4f8feda9aa3bd65e3856
Cris <FilipCristiMihail@Yahoo.com>
2024-10-15 19:48:21 +0200

    #40 Server - Domain: Add CRUD Use cases for Album

--- repertoire.server/domain/usecases/module.go
+++ repertoire.server/domain/usecases/module.go
@@ -1,10 +1,20 @@
 package usecases
 
 import (
-	"go.uber.org/fx"
+	"repertoire/domain/usecases/album"
 	"repertoire/domain/usecases/auth"
 	"repertoire/domain/usecases/song"
 	"repertoire/domain/usecases/user"
+
+	"go.uber.org/fx"
+)
+
+var albumUseCases = fx.Options(
+	fx.Provide(album.NewGetAlbum),
+	fx.Provide(album.NewGetAllAlbums),
+	fx.Provide(album.NewCreateAlbum),
+	fx.Provide(album.NewUpdateAlbum),
+	fx.Provide(album.NewDeleteAlbum),
 )
 
 var authUseCases = fx.Options(
