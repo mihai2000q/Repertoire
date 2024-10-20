617fdfd6c248f27209597b55777d9d21427537b4
Cris <filipcristimihail@yahoo.com>
2024-10-20 12:42:20 +0200

    #55 UI - Songs View: Add Pagination

--- repertoire.ui/src/views/SongsView.tsx
+++ repertoire.ui/src/views/SongsView.tsx
@@ -1,21 +1,31 @@
-import { ReactElement } from 'react'
-import {Group, Pagination, Stack, Title} from "@mantine/core";
-import {useGetSongsQuery} from "../state/api";
-import SongCard from "../components/SongCard";
+import { ReactElement, useState } from 'react'
+import { Box, Group, Loader, Pagination, Space, Stack, Title } from '@mantine/core'
+import { useGetSongsQuery } from '../state/api'
+import SongCard from '../components/SongCard'
 
 function SongsView(): ReactElement {
-
-  const data = useGetSongsQuery({})?.data
+  const [currentPage, setCurrentPage] = useState(1)
+  const { data: songs, isLoading } = useGetSongsQuery({
+    pageSize: 20,
+    currentPage: currentPage
+  })
 
   return (
     <Stack h={'100%'}>
       <Title order={3}>Songs</Title>
-      <Group>
-        {data?.songs.map(song => (
-          <SongCard key={song.id} song={song} />
-        ))}
-      </Group>
-      <Pagination total={data?.totalCount / data?.songs.length} />
+      <Group>{songs?.models.map((song) => <SongCard key={song.id} song={song} />)}</Group>
+      <Space flex={1} />
+      <Box style={{ alignSelf: 'center' }}>
+        {!isLoading ? (
+          <Pagination
+            value={currentPage}
+            onChange={setCurrentPage}
+            total={songs?.totalCount / songs?.models.length}
+          />
+        ) : (
+          <Loader size={25} />
+        )}
+      </Box>
     </Stack>
   )
 }
