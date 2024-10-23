d1cd378b9ff0e051391411ce6114f73de60cd17d
Cris <filipcristimihail@yahoo.com>
2024-10-23 20:34:09 +0200

    #66 UI - New Axios Api

--- repertoire.ui/src/state/api.ts
+++ repertoire.ui/src/state/api.ts
@@ -1,82 +1,68 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import { queryWithRedirection } from './api.query'
-import Song from '../types/models/Song'
-import {
-  CreateSongRequest,
-  GetSongsRequest,
-  UpdateSongRequest
-} from '../types/requests/SongRequests'
-import TokenResponse from '../types/responses/TokenResponse'
-import SignInRequest from '../types/requests/AuthRequests'
-import User from '../types/models/User'
-import HttpMessageResponse from '../types/responses/HttpMessageResponse'
-import WithTotalCountResponse from "../types/responses/WithTotalCountResponse";
+import axios from 'axios'
+import { Mutex } from 'async-mutex'
+import { useNavigate } from 'react-router-dom'
 
-export const api = createApi({
-  baseQuery: queryWithRedirection,
-  reducerPath: 'api',
-  tagTypes: ['Songs'],
-  endpoints: (build) => {
-    return {
-      // Auth
-      signIn: build.mutation<TokenResponse, SignInRequest>({
-        query: (body) => ({
-          url: `auth/sign-in`,
-          method: 'PUT',
-          body: body
-        })
-      }),
-
-      // Users
-      getCurrentUser: build.query<User, undefined>({
-        query: () => `users/current`
-      }),
-
-      // Songs
-      getSongs: build.query<WithTotalCountResponse<Song>, GetSongsRequest>({
-        query: (arg) => ({
-          url: 'songs/',
-          params: arg
-        }),
-        providesTags: ['Songs']
-      }),
-      getSong: build.query<Song, string>({
-        query: (arg) => `songs/${arg}`,
-        providesTags: ['Songs']
-      }),
-      createSong: build.mutation<HttpMessageResponse, CreateSongRequest>({
-        query: (body) => ({
-          url: 'songs/',
-          method: 'POST',
-          body: body
-        }),
-        invalidatesTags: ['Songs']
-      }),
-      updateSong: build.mutation<HttpMessageResponse, UpdateSongRequest>({
-        query: (body) => ({
-          url: 'songs/',
-          method: 'PUT',
-          body: body
-        }),
-        invalidatesTags: ['Songs']
-      }),
-      deleteSong: build.mutation<HttpMessageResponse, string>({
-        query: (arg) => ({
-          url: `songs/${arg}`,
-          method: 'DELETE'
-        }),
-        invalidatesTags: ['Songs']
-      })
-    }
-  }
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL
 })
 
-export const {
-  useSignInMutation,
-  useGetCurrentUserQuery,
-  useGetSongsQuery,
-  useGetSongQuery,
-  useCreateSongMutation,
-  useUpdateSongMutation,
-  useDeleteSongMutation
-} = api
+// Authorization
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+// Refresh Token
+const mutex = new Mutex()
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    await mutex.waitForUnlock()
+    console.log(error)
+    if (error.response?.status === 401) {
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire()
+        try {
+          const token = localStorage.getItem('token')
+          const refresh = await axios.put('/auth/refresh/', { token })
+          const data = refresh.data as { token: string } | undefined
+          if (data) {
+            localStorage.setItem('token', token)
+            error = await axios.request(error.config)
+          } else {
+            localStorage.removeItem('token')
+          }
+        } finally {
+          release()
+        }
+      } else {
+        await mutex.waitForUnlock()
+        error = await axios.request(error.config)
+      }
+    }
+    return error
+  }
+)
+
+const errorCodeToPathname = new Map<number | string, string>([
+  [403, '401'],
+  [404, '404']
+])
+
+// Error Redirection
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const errorStatus = error.response?.status ?? 0
+    const navigate = useNavigate()
+    if (errorCodeToPathname.has(errorStatus)) {
+      navigate(errorCodeToPathname.get(errorStatus))
+    }
+    return error
+  }
+)
+
+function isAuthRequest(args: string): boolean {
+  return typeof args === 'string' && args.includes('auth')
+}
