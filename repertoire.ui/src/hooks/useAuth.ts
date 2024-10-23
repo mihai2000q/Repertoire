4db7c6c35e05fddcff592c45cb77c4a1f13153e1
Cris <filipcristimihail@yahoo.com>
2024-10-23 20:33:44 +0200

    #66 UI - Use Auth Hook - remove Redux dependency

--- repertoire.ui/src/hooks/useAuth.ts
+++ repertoire.ui/src/hooks/useAuth.ts
@@ -1,6 +1,4 @@
-import { useAppSelector } from '../state/store'
-
 export default function useAuth(): boolean {
-  const token = useAppSelector((state) => state.auth.token)
+  const token = localStorage.getItem('token')
   return token !== null
 }
