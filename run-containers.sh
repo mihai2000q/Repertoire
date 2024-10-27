45c2136fe9133f84d533f3231c9415412000a3c2
Cris <FilipCristiMihail@Yahoo.com>
2024-10-27 12:06:06 +0100

    #78 Run Containers Shell Script - Include Web Container too

--- run-containers.sh
+++ run-containers.sh
@@ -1,3 +1,4 @@
 DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker-compose -f ${DIR_SCRIPT}/repertoire.server/docker-compose.yml -d up
+docker-compose -f ${DIR_SCRIPT}/repertoire.server/docker-compose.yaml up -d
+docker-compose -f ${DIR_SCRIPT}/repertoire.web/docker-compose.yaml up -d
