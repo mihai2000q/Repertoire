2125048a73bd455a349cca0ffc21b89c6e895760
Cris <filipcristimihail@yahoo.com>
2024-10-29 13:40:14 +0100

    #95 UI - New Songs Component: Song Drawer

---
+++ repertoire.ui/src/components/songs/SongDrawer.test.tsx
@@ -0,0 +1,42 @@
+import {reduxRender} from "../../test-utils.tsx";
+import SongDrawer from "./SongDrawer.tsx";
+import {screen} from "@testing-library/react";
+import {http, HttpResponse} from "msw";
+import Song from "../../types/models/Song.ts";
+import {setupServer} from "msw/node";
+
+describe('Song Drawer', () => {
+  const song: Song = {
+    id: '1',
+    title: "Justice for all",
+    isRecorded: false,
+  }
+
+  const handlers = [
+    http.get('/songs/1', async () => {
+      return HttpResponse.json(song)
+    })
+  ]
+
+  const server = setupServer(...handlers)
+
+  beforeAll(() => server.listen())
+
+  afterEach(() => server.resetHandlers())
+
+  afterAll(() => server.close())
+
+  it('should render and display the loader', () => {
+    reduxRender(<SongDrawer opened={true} close={() => {}} />)
+
+    expect(screen.getByTestId('song-drawer-loader')).toBeInTheDocument()
+  })
+
+  it('should display song details when the songId exists', async () => {
+    reduxRender(<SongDrawer opened={true} close={() => {}} />, { songs: { songId: '1' } })
+
+    expect(await screen.findByText(song.title)).toBeInTheDocument()
+    
+    expect(screen.queryByTestId('song-drawer-loader')).not.toBeInTheDocument()
+  })
+})
