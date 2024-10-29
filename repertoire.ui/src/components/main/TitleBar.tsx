1cec36ddcf31035bc3c066b725e782c513363a8e
Cris <filipcristimihail@yahoo.com>
2024-10-29 11:27:20 +0100

    #88 UI - Main: Titlebar - Unit Testing

--- repertoire.ui/src/components/main/TitleBar.tsx
+++ repertoire.ui/src/components/main/TitleBar.tsx
@@ -16,27 +16,28 @@ function TitleBar() {
 
   return (
     <Stack
+      data-testid="title-bar"
       bg={'white'}
       pos={'fixed'}
       top={0}
       w={'100%'}
       gap={0}
-      style={{ webkitAppRegion: 'drag' }}
+      style={{ WebkitAppRegion: 'drag' }}
     >
       <Group gap={0} h={45} px={'xs'} align={'center'}>
-        <Avatar />
-        <Text>Repertoire</Text>
+        <Avatar size={35} />
+        <Text pl={4}>Repertoire</Text>
 
         <Space flex={1} />
 
-        <Group gap={4} style={{ webkitAppRegion: 'no-drag' }}>
-          <ActionIcon variant={'subtle'} size={'lg'} onClick={handleMinimize}>
+        <Group gap={4} style={{ WebkitAppRegion: 'no-drag' }}>
+          <ActionIcon aria-label={'minimize'} variant={'subtle'} size={'lg'} onClick={handleMinimize}>
             <IconArrowDownLeft />
           </ActionIcon>
-          <ActionIcon variant={'subtle'} size={'lg'} onClick={handleMaximize}>
+          <ActionIcon aria-label={'maximize'} variant={'subtle'} size={'lg'} onClick={handleMaximize}>
             <IconArrowsDiagonal />
           </ActionIcon>
-          <ActionIcon variant={'subtle'} size={'lg'} onClick={handleClose}>
+          <ActionIcon aria-label={'close'} variant={'subtle'} size={'lg'} onClick={handleClose}>
             <IconX />
           </ActionIcon>
         </Group>
