a53a1ab4d736156ad97d19d0b368b17694a7eb96
Cris <filipcristimihail@yahoo.com>
2024-10-29 13:56:04 +0100

    #95 UI - Song Component: Song Card - Open Drawer on click and dispatch new song id

--- repertoire.ui/src/components/songs/SongCard.test.tsx
+++ repertoire.ui/src/components/songs/SongCard.test.tsx
@@ -1,7 +1,9 @@
 import { screen } from '@testing-library/react'
 import SongCard from './SongCard'
 import Song from '../../types/models/Song'
-import { mantineRender } from '../../test-utils'
+import { reduxRender } from '../../test-utils'
+import { userEvent } from '@testing-library/user-event'
+import { RootState } from '../../state/store.ts'
 
 describe('Song Card', () => {
   it('should render and display minimal info', ({ expect }) => {
@@ -13,7 +15,7 @@ describe('Song Card', () => {
     }
 
     // Act
-    const { container } = mantineRender(<SongCard song={song} />)
+    const [{ container }] = reduxRender(<SongCard song={song} openDrawer={() => {}} />)
 
     // Assert
     expect(screen.getByText(song.title)).toBeVisible()
@@ -29,10 +31,31 @@ describe('Song Card', () => {
     }
 
     // Act
-    const { container } = mantineRender(<SongCard song={song} />)
+    const [{ container }] = reduxRender(<SongCard song={song} openDrawer={() => {}} />)
 
     // Assert
     expect(screen.getByText(song.title)).toBeVisible()
     expect(container.querySelector('svg.tabler-icon-microphone-filled')).toBeVisible()
+  })
+
+  it('should open drawer and dispatch song Id on click', async ({ expect }) => {
+    // Arrange
+    const song: Song = {
+      id: '1',
+      title: 'Some song',
+      isRecorded: true
+    }
+
+    const user = userEvent.setup()
+    const openDrawer = vi.fn()
+
+    // Act
+    const [_, store] = reduxRender(<SongCard song={song} openDrawer={openDrawer} />)
+
+    // Assert
+    await user.click(screen.getByTestId(`song-card-${song.id}`))
+
+    expect(openDrawer).toHaveBeenCalledOnce()
+    expect((store.getState() as RootState).songs.songId).toBe(song.id)
   })
 })
