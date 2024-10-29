a0a16fca942eb987b188c905cd67d165336e3f6a
Cris <filipcristimihail@yahoo.com>
2024-10-29 14:24:13 +0100

    #97 Server - Utils - New Enum: Difficulty

---
+++ repertoire.server/utils/enums/Difficulty.go
@@ -0,0 +1,10 @@
+package enums
+
+type Difficulty string
+
+const (
+	Easy       Difficulty = "easy"
+	Medium     Difficulty = "medium"
+	Hard       Difficulty = "hard"
+	Impossible Difficulty = "impossible"
+)
