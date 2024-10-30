89712416c1143184c7eb5dbb755233a8839ad8ae
Cris <filipcristimihail@yahoo.com>
2024-10-30 11:00:45 +0100

    #101 Add Root .Scripts + Startup storage

---
+++ .scripts/setup-ui.sh
@@ -0,0 +1,10 @@
+DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR_SCRIPT}/../repertoire.ui
+
+echo "Installing UI Dependencies"
+npm ci
+
+echo -e "UI Setup Finished\n"
+
+cd ..
