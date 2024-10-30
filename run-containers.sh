29e5eee4f23ecd378a11a39b138ed1611fd6ff8d
Cris <filipcristimihail@yahoo.com>
2024-10-30 11:00:58 +0100

    #101 Storage - Add Docker Support

--- run-containers.sh
+++ run-containers.sh
@@ -1,4 +1,5 @@
 DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker-compose -f ${DIR_SCRIPT}/repertoire.server/docker-compose.yaml up -d
+docker-compose -f ${DIR_SCRIPT}/repertoire.storage/docker-compose.yaml up -d
 docker-compose -f ${DIR_SCRIPT}/repertoire.web/docker-compose.yaml up -d
