5065dbeb3dadb14a2285456326901c86b375f804
Cris <FilipCristiMihail@Yahoo.com>
2024-11-05 14:25:58 +0100

    #117 Server - Playlist - Add Image Url

--- repertoire.server/model/playlist.go
+++ repertoire.server/model/playlist.go
@@ -1,15 +1,18 @@
 package model
 
 import (
-	"github.com/google/uuid"
+	"repertoire/server/internal"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Playlist struct {
-	ID          uuid.UUID `gorm:"primaryKey; type:uuid; <-:create" json:"id"`
-	Title       string    `gorm:"size:100; not null" json:"title"`
-	Description string    `gorm:"not null" json:"description"`
-	Songs       []Song    `gorm:"many2many:playlist_song" json:"songs"`
+	ID          uuid.UUID          `gorm:"primaryKey; type:uuid; <-:create" json:"id"`
+	Title       string             `gorm:"size:100; not null" json:"title"`
+	Description string             `gorm:"not null" json:"description"`
+	ImageURL    *internal.FilePath `json:"imageUrl"`
+	Songs       []Song             `gorm:"many2many:playlist_song" json:"songs"`
 
 	CreatedAt time.Time `gorm:"default:current_timestamp; not null; <-:create" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"default:current_timestamp; not null" json:"updatedAt"`
