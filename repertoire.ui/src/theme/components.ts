9bcc386770131c3be3e3df13a91f2c215ac42ba8
Cris <filipcristimihail@yahoo.com>
2024-11-06 11:45:22 +0100

    #120 UI - Adjust Sidebar Theme

--- repertoire.ui/src/theme/components.ts
+++ repertoire.ui/src/theme/components.ts
@@ -1,4 +1,4 @@
-import {NavLink} from "@mantine/core";
+import {alpha, NavLink} from "@mantine/core";
 
 export const components = {
   NavLink: NavLink.extend({
@@ -7,7 +7,29 @@ export const components = {
       px: 'lg',
       style: {
         borderRadius: '16px',
-      }
+      },
+      styles: (theme) => ({
+        root: {
+          transition: '0.25s',
+          color: theme.colors.gray[6],
+          '&:hover': {
+            backgroundColor: 'inherit',
+            color: theme.colors.gray[7],
+            transform: 'scale(110%)',
+          },
+
+          '&:where([data-active])': {
+            backgroundColor: alpha(theme.colors.cyan[4], 0.1),
+            color: theme.colors.cyan[4],
+
+            '&:hover': {
+              backgroundColor: alpha(theme.colors.cyan[4], 0.15),
+              color: theme.colors.cyan[5],
+              transform: 'scale(100%)'
+            },
+          }
+        },
+      })
     },
   }),
 }
