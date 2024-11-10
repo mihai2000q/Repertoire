cfdeb1f198ac264b449c742ed7b5616ac2928905
Cris <filipcristimihail@yahoo.com>
2024-11-10 12:02:29 +0100

    #120 UI - Songs Form Validation - Add Missing Elements

--- repertoire.ui/src/validation/songsForm.ts
+++ repertoire.ui/src/validation/songsForm.ts
@@ -1,9 +1,36 @@
-import {z} from "zod";
+import { z } from 'zod'
 
 export interface AddNewSongForm {
   title: string
+  description: string
+
+  albumId?: string
+  albumTitle?: string
+  artistId?: string
+  artistName?: string
+
+  releaseDate?: Date
+  bpm?: number
+  songsterrLink?: string
+  youtubeLink?: string
 }
 
 export const addNewSongValidation = z.object({
-  title: z.string().trim().min(1, "Title cannot be blank"),
+  title: z.string().trim().min(1, 'Title cannot be blank'),
+  songsterrLink: z
+    .preprocess(
+      (input) => (input === '' ? null : input),
+      z
+        .string()
+        .includes('songsterr.com', { message: 'This is not a valid Songsterr URL' })
+        .url('This is not a valid URL')
+        .nullish()
+    )
+    .nullish(),
+  youtubeLink: z
+    .preprocess(
+      (input) => (input === '' ? null : input),
+      z.string().url('This is not a valid URL').nullish()
+    )
+    .nullish()
 })
