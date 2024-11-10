e72540ce3ba5258a98482fb5c9dce2c991f0fa0e
Cris <filipcristimihail@yahoo.com>
2024-11-10 12:15:14 +0100

    #120 UI - Emotion.d.ts - Ignore unused variable

--- repertoire.ui/src/emotion.d.ts
+++ repertoire.ui/src/emotion.d.ts
@@ -3,6 +3,7 @@ import '@mantine/core'
 import type { EmotionStyles, EmotionSx } from '@mantine/emotion'
 
 declare module '@mantine/core' {
+  // noinspection JSUnusedGlobalSymbols
   export interface BoxProps {
     sx?: EmotionSx
     styles?: EmotionStyles
