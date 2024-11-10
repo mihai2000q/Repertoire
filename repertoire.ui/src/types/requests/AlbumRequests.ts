bed3d4d83a0b29fa3bee8bdc76a9b67b9fd5032b
Cris <filipcristimihail@yahoo.com>
2024-11-10 14:30:13 +0100

    #127 UI - New Album Requests Type + Album Api completed (Get, Create, Update, Save Image and Delete Endpoints)

--- repertoire.ui/src/types/requests/AlbumRequests.ts
+++ repertoire.ui/src/types/requests/AlbumRequests.ts
@@ -1,4 +1,24 @@
+import {FileWithPath} from "@mantine/dropzone";
+
 export interface GetAlbumsRequest {
   currentPage?: number
   pageSize?: number
+}
+
+export interface CreateAlbumRequest {
+  title: string
+  releaseDate: string
+  artistId?: string
+  artistName?: string
+}
+
+export interface UpdateAlbumRequest {
+  id: string
+  title: string
+  releaseDate: string
+}
+
+export interface SaveImageToAlbumRequest {
+  image: FileWithPath
+  id: string
 }
