deacf123470c921fe109e8260db620467d9b1583
Cris <filipcristimihail@yahoo.com>
2024-11-10 14:59:35 +0100

    #131 UI - Complete Artist Requests + Api (Create, Update, Delete, Get and Save Image Endpoints)

--- repertoire.ui/src/state/artistsApi.ts
+++ repertoire.ui/src/state/artistsApi.ts
@@ -1,7 +1,14 @@
 import { api } from './api'
 import WithTotalCountResponse from '../types/responses/WithTotalCountResponse'
-import Artist from '../types/models/Artist.ts'
-import { GetArtistsRequest } from '../types/requests/ArtistRequests.ts'
+import Artist from "../types/models/Artist.ts";
+import {
+  CreateArtistRequest,
+  GetArtistsRequest,
+  SaveImageToArtistRequest,
+  UpdateArtistRequest
+} from "../types/requests/ArtistRequests.ts";
+import HttpMessageResponse from "../types/responses/HttpMessageResponse.ts";
+import createFormData from "../utils/createFormData.ts";
 
 const songsApi = api.injectEndpoints({
   endpoints: (build) => ({
@@ -11,6 +18,42 @@ const songsApi = api.injectEndpoints({
         params: arg
       }),
       providesTags: ['Artists']
+    }),
+    getArtist: build.query<Artist, string>({
+      query: (arg) => `artists/${arg}`,
+      providesTags: ['Artists']
+    }),
+    createArtist: build.mutation<{ id: string }, CreateArtistRequest>({
+      query: (body) => ({
+        url: 'artists',
+        method: 'POST',
+        body: body
+      }),
+      invalidatesTags: ['Artists']
+    }),
+    updateArtist: build.mutation<HttpMessageResponse, UpdateArtistRequest>({
+      query: (body) => ({
+        url: 'artists',
+        method: 'PUT',
+        body: body
+      }),
+      invalidatesTags: ['Artists']
+    }),
+    saveImageToArtist: build.mutation<HttpMessageResponse, SaveImageToArtistRequest>({
+      query: (request) => ({
+        url: 'artists/images',
+        method: 'PUT',
+        body: createFormData(request),
+        formData: true
+      }),
+      invalidatesTags: ['Artists']
+    }),
+    deleteArtist: build.mutation<HttpMessageResponse, string>({
+      query: (arg) => ({
+        url: `artists/${arg}`,
+        method: 'DELETE'
+      }),
+      invalidatesTags: ['Artists']
     })
   })
 })
