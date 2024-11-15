d086c8b563b03bf2289c1fce8260336eb41a95c9
Cris <filipcristimihail@yahoo.com>
2024-11-15 21:04:27 +0100

    #150 UI - Edit Panel Card Component - Add Tooltip to Action Icon

--- repertoire.ui/src/components/card/EditPanelCard.tsx
+++ repertoire.ui/src/components/card/EditPanelCard.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Box, Card } from '@mantine/core'
+import {ActionIcon, Box, Card, Tooltip} from '@mantine/core'
 import { ReactElement } from 'react'
 import { IconPencil } from '@tabler/icons-react'
 import {useHover} from "@mantine/hooks";
@@ -17,14 +17,16 @@ function EditPanelCard({ children, onEditClick, p }: EditPanelCardProps) {
       {children}
 
       <Box pos={'absolute'} right={0} top={0} p={4}>
-        <ActionIcon
-          variant={'grey'}
-          size={'md'}
-          style={{ transition: '0.25s', opacity: hovered ? 1 : 0 }}
-          onClick={onEditClick}
-        >
-          <IconPencil size={18} />
-        </ActionIcon>
+        <Tooltip label={'Edit Panel'} openDelay={500}>
+          <ActionIcon
+            variant={'grey'}
+            size={'md'}
+            style={{ transition: '0.25s', opacity: hovered ? 1 : 0 }}
+            onClick={onEditClick}
+          >
+            <IconPencil size={18} />
+          </ActionIcon>
+        </Tooltip>
       </Box>
     </Card>
   )
