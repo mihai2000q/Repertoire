94adac909aebd4f58e9a264d99e4332cc40f6b49
Cris <filipcristimihail@yahoo.com>
2024-11-18 11:22:38 +0100

    #153 UI - New Hook: use Context Menu

---
+++ repertoire.ui/src/hooks/useContextMenu.ts
@@ -0,0 +1,47 @@
+import { MouseEvent, useState } from 'react'
+import { MantineStyleProp } from '@mantine/core'
+
+interface MenuState {
+  opened: boolean
+  position?: {
+    top: number
+    left: number
+  }
+}
+
+interface MenuProps {
+  style: MantineStyleProp
+}
+
+interface ContextMenuHandlers {
+  openMenu: (event: MouseEvent) => void
+  onChange: (opened: boolean) => void
+}
+
+export default function useContextMenu(): [MenuState, MenuProps, ContextMenuHandlers] {
+  const [menuState, setMenuState] = useState<MenuState>({ opened: false })
+  const menuProps: MenuProps = {
+    style: {
+      position: 'absolute',
+      top: menuState.position?.top,
+      left: menuState.position?.left
+    }
+  }
+
+  function openMenu(e: MouseEvent) {
+    e.preventDefault()
+    setMenuState({
+      opened: true,
+      position: {
+        top: e.clientY,
+        left: e.clientX
+      }
+    })
+  }
+
+  function onChange(opened: boolean) {
+    setMenuState({ ...menuState, opened })
+  }
+
+  return [menuState, menuProps, { openMenu, onChange }]
+}
