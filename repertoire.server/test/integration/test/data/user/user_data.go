926209ad3d660cd7de90a6882c80281cbb6a01db
Cris <filipcristimihail@yahoo.com>
2024-11-28 15:59:59 +0100

    #155 Server - Integration Testing - Add more test data on user handler tests

--- repertoire.server/test/integration/test/data/user/user_data.go
+++ repertoire.server/test/integration/test/data/user/user_data.go
@@ -9,6 +9,10 @@ import (
 
 func SeedData(db *gorm.DB) {
 	db.Create(&Users)
+	db.Create(&Artists)
+	db.Create(&Albums)
+	db.Create(&Songs)
+	db.Create(&Playlists)
 }
 
 var Users = []model.User{
@@ -24,5 +28,37 @@ var Users = []model.User{
 		Email:             "vivaldi@gmail.com",
 		Password:          "",
 		ProfilePictureURL: &[]internal.FilePath{"userId/Some image path/somewhere.jpeg"}[0],
+	},
+}
+
+var Artists = []model.Artist{
+	{
+		ID:     uuid.New(),
+		Name:   "Some Artist",
+		UserID: Users[0].ID,
+	},
+}
+
+var Albums = []model.Album{
+	{
+		ID:     uuid.New(),
+		Title:  "Some Album",
+		UserID: Users[0].ID,
+	},
+}
+
+var Songs = []model.Song{
+	{
+		ID:     uuid.New(),
+		Title:  "Some Song",
+		UserID: Users[0].ID,
+	},
+}
+
+var Playlists = []model.Playlist{
+	{
+		ID:     uuid.New(),
+		Title:  "Some Playlist",
+		UserID: Users[0].ID,
 	},
 }
