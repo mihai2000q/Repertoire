c746775635bbee131e94d60cfb2ac79c029451f8
Cris <FilipCristiMihail@Yahoo.com>
2024-11-30 15:04:36 +0100

    #165 Server - Integration Testing - Add Test Utils and Assertions to create and assert tokens

--- repertoire.server/test/integration/test/utils/test_utils.go
+++ repertoire.server/test/integration/test/utils/test_utils.go
@@ -1,13 +1,17 @@
 package utils
 
 import (
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 	"repertoire/server/internal"
 	"repertoire/server/model"
 	"repertoire/server/test/integration/test/core"
 	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func GetDatabase() *gorm.DB {
@@ -17,6 +21,38 @@ func GetDatabase() *gorm.DB {
 
 func GetEnv() internal.Env {
 	return internal.NewEnv()
+}
+
+func CreateValidToken(user model.User) string {
+	env := GetEnv()
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"jti": uuid.New().String(),
+		"sub": user.ID.String(),
+		"iss": env.JwtIssuer,
+		"aud": env.JwtAudience,
+		"iat": time.Now().UTC().Unix(),
+		"exp": time.Now().UTC().Add(time.Hour).Unix(),
+	})
+	token, _ := claims.SignedString([]byte(env.JwtSecretKey))
+
+	return token
+}
+
+func CreateCustomToken(sub string, jti string) string {
+	env := GetEnv()
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"jti": jti,
+		"sub": sub,
+		"iss": env.JwtIssuer,
+		"aud": env.JwtAudience,
+		"iat": time.Now().UTC().Unix(),
+		"exp": time.Now().UTC().Add(time.Hour).Unix(),
+	})
+	token, _ := claims.SignedString([]byte(env.JwtSecretKey))
+
+	return token
 }
 
 func SeedAndCleanupData(t *testing.T, users []model.User, seed func(*gorm.DB)) {
