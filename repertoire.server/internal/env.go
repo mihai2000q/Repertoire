6dbbe28e21388f2100ba3ab39c3039223ff37243
Cris <filipcristimihail@yahoo.com>
2024-12-05 22:39:18 +0100

    #190 Server - Environment Variables - Set The Storage Client ID and Client Secret

--- repertoire.server/internal/env.go
+++ repertoire.server/internal/env.go
@@ -25,7 +25,9 @@ type Env struct {
 	JwtAudience  string
 	JwtSecretKey string
 
-	StorageUrl string
+	StorageUrl          string
+	StorageClientID     string
+	StorageClientSecret string
 }
 
 func NewEnv() Env {
@@ -39,21 +41,27 @@ func NewEnv() Env {
 	}
 
 	env := Env{
-		ApplicationHost:  os.Getenv("SERVER_HOST"),
-		ApplicationPort:  os.Getenv("SERVER_PORT"),
-		Environment:      os.Getenv("ENV"),
+		ApplicationHost: os.Getenv("SERVER_HOST"),
+		ApplicationPort: os.Getenv("SERVER_PORT"),
+		Environment:     os.Getenv("ENV"),
+
 		DatabaseHost:     os.Getenv("DB_HOST"),
 		DatabaseUser:     os.Getenv("DB_USER"),
 		DatabasePassword: os.Getenv("DB_PASSWORD"),
 		DatabaseName:     os.Getenv("DB_NAME"),
 		DatabasePort:     os.Getenv("DB_PORT"),
 		DatabaseSSLMode:  os.Getenv("DB_SSL_MODE"),
-		LogOutput:        os.Getenv("LOG_OUTPUT"),
-		LogLevel:         os.Getenv("LOG_LEVEL"),
-		JwtIssuer:        os.Getenv("JWT_ISSUER"),
-		JwtAudience:      os.Getenv("JWT_AUDIENCE"),
-		JwtSecretKey:     os.Getenv("JWT_SECRET_KEY"),
-		StorageUrl:       os.Getenv("UPLOAD_STORAGE_URL"),
+
+		LogOutput: os.Getenv("LOG_OUTPUT"),
+		LogLevel:  os.Getenv("LOG_LEVEL"),
+
+		JwtIssuer:    os.Getenv("JWT_ISSUER"),
+		JwtAudience:  os.Getenv("JWT_AUDIENCE"),
+		JwtSecretKey: os.Getenv("JWT_SECRET_KEY"),
+
+		StorageUrl:          os.Getenv("UPLOAD_STORAGE_URL"),
+		StorageClientID:     os.Getenv("STORAGE_CLIENT_ID"),
+		StorageClientSecret: os.Getenv("STORAGE_CLIENT_SECRET"),
 	}
 	return env
 }
