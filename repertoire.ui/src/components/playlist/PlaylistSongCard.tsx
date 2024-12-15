1a818c98405b8a994f23fd7a5e984d75a6df623a
Cris <filipcristimihail@yahoo.com>
2024-12-15 19:52:49 +0100

    #223 UI - Playlist - Songs - When removing a song, display warning modal

--- repertoire.ui/src/components/playlist/PlaylistSongCard.tsx
+++ repertoire.ui/src/components/playlist/PlaylistSongCard.tsx
@@ -3,9 +3,10 @@ import { ActionIcon, alpha, Avatar, Group, Menu, Stack, Text } from '@mantine/co
 import songPlaceholder from '../../assets/image-placeholder-1.jpg'
 import { useAppDispatch } from '../../state/store.ts'
 import { openSongDrawer } from '../../state/globalSlice.ts'
-import { useHover } from '@mantine/hooks'
+import { useDisclosure, useHover } from '@mantine/hooks'
 import { MouseEvent, useState } from 'react'
 import { IconDots, IconTrash } from '@tabler/icons-react'
+import WarningModal from '../modal/WarningModal.tsx'
 
 interface PlaylistSongCardProps {
   song: Song
@@ -20,78 +21,102 @@ function PlaylistSongCard({ song, handleRemove }: PlaylistSongCardProps) {
 
   const isSelected = hovered || isMenuOpened
 
+  const [openedRemoveWarning, { open: openRemoveWarning, close: closeRemoveWarning }] =
+    useDisclosure(false)
+
   function handleClick() {
     dispatch(openSongDrawer(song.id))
   }
 
-  function handleDelete(e: MouseEvent) {
+  function handleOpenRemoveWarning(e: MouseEvent) {
     e.stopPropagation()
-    handleRemove()
+    openRemoveWarning()
   }
 
   return (
-    <Group
-      ref={ref}
-      align={'center'}
-      wrap={'nowrap'}
-      sx={(theme) => ({
-        cursor: 'default',
-        transition: '0.3s',
-        '&:hover': {
-          boxShadow: theme.shadows.xl,
-          backgroundColor: alpha(theme.colors.cyan[0], 0.15)
-        }
-      })}
-      px={'md'}
-      py={'xs'}
-      onClick={handleClick}
-    >
-      <Text fw={500} w={35} ta={'center'}>
-        {song.playlistTrackNo}
-      </Text>
+    <>
+      <Group
+        ref={ref}
+        align={'center'}
+        wrap={'nowrap'}
+        sx={(theme) => ({
+          cursor: 'default',
+          transition: '0.3s',
+          '&:hover': {
+            boxShadow: theme.shadows.xl,
+            backgroundColor: alpha(theme.colors.cyan[0], 0.15)
+          }
+        })}
+        px={'md'}
+        py={'xs'}
+        onClick={handleClick}
+      >
+        <Text fw={500} w={35} ta={'center'}>
+          {song.playlistTrackNo}
+        </Text>
 
-      <Avatar radius={'8px'} src={song.imageUrl ?? songPlaceholder} />
+        <Avatar radius={'8px'} src={song.imageUrl ?? songPlaceholder} />
 
-      <Stack flex={1} gap={0} style={{ overflow: 'hidden' }}>
-        <Group gap={4}>
-          <Text fw={500} truncate={'end'}>
-            {song.title}
-          </Text>
-          {song.album && (
-            <Text fz={'sm'} c={'dimmed'} truncate={'end'}>
-              - {song.album.title}
+        <Stack flex={1} gap={0} style={{ overflow: 'hidden' }}>
+          <Group gap={4}>
+            <Text fw={500} truncate={'end'}>
+              {song.title}
+            </Text>
+            {song.album && (
+              <Text fz={'sm'} c={'dimmed'} truncate={'end'}>
+                - {song.album.title}
+              </Text>
+            )}
+          </Group>
+          {song.artist && (
+            <Text fz={'sm'} c={'dimmed'}>
+              {song.artist.name}
             </Text>
           )}
-        </Group>
-        {song.artist && (
-          <Text fz={'sm'} c={'dimmed'}>
-            {song.artist.name}
-          </Text>
-        )}
-      </Stack>
+        </Stack>
 
-      <Menu position={'bottom-end'} opened={isMenuOpened} onChange={setIsMenuOpened}>
-        <Menu.Target>
-          <ActionIcon
-            size={'md'}
-            variant={'grey'}
-            onClick={(e) => e.stopPropagation()}
-            style={{
-              transition: '0.3s',
-              opacity: isSelected ? 1 : 0
-            }}
-          >
-            <IconDots size={15} />
-          </ActionIcon>
-        </Menu.Target>
+        <Menu position={'bottom-end'} opened={isMenuOpened} onChange={setIsMenuOpened}>
+          <Menu.Target>
+            <ActionIcon
+              size={'md'}
+              variant={'grey'}
+              onClick={(e) => e.stopPropagation()}
+              style={{
+                transition: '0.3s',
+                opacity: isSelected ? 1 : 0
+              }}
+            >
+              <IconDots size={15} />
+            </ActionIcon>
+          </Menu.Target>
 
-        <Menu.Dropdown>
-          <Menu.Item leftSection={<IconTrash size={14} />} c={'red.5'} onClick={handleDelete}>
-            Remove from Playlist
-          </Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
-    </Group>
+          <Menu.Dropdown>
+            <Menu.Item
+              leftSection={<IconTrash size={14} />}
+              c={'red.5'}
+              onClick={handleOpenRemoveWarning}
+            >
+              Remove from Playlist
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </Group>
+      <WarningModal
+        opened={openedRemoveWarning}
+        onClose={closeRemoveWarning}
+        title={`Remove Song`}
+        description={
+          <Stack gap={4}>
+            <Group gap={4}>
+              <Text>Are you sure you want to remove</Text>
+              <Text fw={600}>{song.title}</Text>
+              <Text>from this playlist?</Text>
+            </Group>
+          </Stack>
+        }
+        onYes={handleRemove}
+      />
+    </>
   )
 }
 
