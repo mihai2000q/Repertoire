db2d0c0061f5955fc9c5b05658a4ecf9449f6822
Cris <filipcristimihail@yahoo.com>
2024-12-15 19:08:28 +0100

    #223 UI - Artist - Album & Song - When removing from artist, display warning modal

--- repertoire.ui/src/components/artist/ArtistAlbumCard.tsx
+++ repertoire.ui/src/components/artist/ArtistAlbumCard.tsx
@@ -5,8 +5,9 @@ import dayjs from 'dayjs'
 import { useAppDispatch } from '../../state/store.ts'
 import { openAlbumDrawer } from '../../state/globalSlice.ts'
 import { MouseEvent, useState } from 'react'
-import { useHover } from '@mantine/hooks'
+import { useDisclosure, useHover } from '@mantine/hooks'
 import { IconDots, IconTrash } from '@tabler/icons-react'
+import WarningModal from '../modal/WarningModal.tsx'
 
 interface ArtistAlbumCardProps {
   album: Album
@@ -20,69 +21,97 @@ function ArtistAlbumCard({ album, handleRemove, isUnknownArtist }: ArtistAlbumCa
   const [isMenuOpened, setIsMenuOpened] = useState(false)
   const isSelected = hovered || isMenuOpened
 
+  const [openedRemoveWarning, { open: openRemoveWarning, close: closeRemoveWarning }] =
+    useDisclosure(false)
+
   function handleClick() {
     dispatch(openAlbumDrawer(album.id))
   }
 
-  function handleDelete(e: MouseEvent) {
+  function handleOpenRemoveWarning(e: MouseEvent) {
     e.stopPropagation()
-    handleRemove()
+    openRemoveWarning()
   }
 
   return (
-    <Group
-      ref={ref}
-      align={'center'}
-      wrap={'nowrap'}
-      sx={(theme) => ({
-        cursor: 'default',
-        borderRadius: '8px',
-        transition: '0.3s',
-        ...(isSelected && {
-          boxShadow: theme.shadows.xl,
-          backgroundColor: alpha(theme.colors.cyan[0], 0.15)
-        })
-      })}
-      px={'md'}
-      py={'xs'}
-      onClick={handleClick}
-    >
-      <Avatar radius={'8px'} src={album.imageUrl ?? albumPlaceholder} />
+    <>
+      <Group
+        ref={ref}
+        align={'center'}
+        wrap={'nowrap'}
+        sx={(theme) => ({
+          cursor: 'default',
+          borderRadius: '8px',
+          transition: '0.3s',
+          ...(isSelected && {
+            boxShadow: theme.shadows.xl,
+            backgroundColor: alpha(theme.colors.cyan[0], 0.15)
+          })
+        })}
+        px={'md'}
+        py={'xs'}
+        onClick={handleClick}
+      >
+        <Avatar radius={'8px'} src={album.imageUrl ?? albumPlaceholder} />
 
-      <Stack gap={0} flex={1} style={{ overflow: 'hidden' }}>
-        <Text fw={500} truncate={'end'}>
-          {album.title}
-        </Text>
-        {album.releaseDate && (
-          <Text fz={'xs'} c={'dimmed'}>
-            {dayjs(album.releaseDate).format('DD MMM YYYY')}
+        <Stack gap={0} flex={1} style={{ overflow: 'hidden' }}>
+          <Text fw={500} truncate={'end'}>
+            {album.title}
           </Text>
-        )}
-      </Stack>
-
-      <Menu position={'bottom-end'} opened={isMenuOpened} onChange={setIsMenuOpened}>
-        <Menu.Target>
-          <ActionIcon
-            size={'md'}
-            variant={'grey'}
-            onClick={(e) => e.stopPropagation()}
-            style={{
-              transition: '0.3s',
-              opacity: isSelected ? 1 : 0
-            }}
-          >
-            <IconDots size={15} />
-          </ActionIcon>
-        </Menu.Target>
-        <Menu.Dropdown>
-          {!isUnknownArtist && (
-            <Menu.Item leftSection={<IconTrash size={14} />} c={'red.5'} onClick={handleDelete}>
-              Remove from Artist
-            </Menu.Item>
+          {album.releaseDate && (
+            <Text fz={'xs'} c={'dimmed'}>
+              {dayjs(album.releaseDate).format('DD MMM YYYY')}
+            </Text>
           )}
-        </Menu.Dropdown>
-      </Menu>
-    </Group>
+        </Stack>
+
+        <Menu position={'bottom-end'} opened={isMenuOpened} onChange={setIsMenuOpened}>
+          <Menu.Target>
+            <ActionIcon
+              size={'md'}
+              variant={'grey'}
+              onClick={(e) => e.stopPropagation()}
+              style={{
+                transition: '0.3s',
+                opacity: isSelected ? 1 : 0
+              }}
+            >
+              <IconDots size={15} />
+            </ActionIcon>
+          </Menu.Target>
+          <Menu.Dropdown>
+            {!isUnknownArtist && (
+              <Menu.Item
+                leftSection={<IconTrash size={14} />}
+                c={'red.5'}
+                onClick={handleOpenRemoveWarning}
+              >
+                Remove from Artist
+              </Menu.Item>
+            )}
+          </Menu.Dropdown>
+        </Menu>
+      </Group>
+
+      <WarningModal
+        opened={openedRemoveWarning}
+        onClose={closeRemoveWarning}
+        title={`Remove ${album.title}`}
+        description={
+          <Stack gap={4}>
+            <Group gap={4}>
+              <Text>Are you sure you want to remove</Text>
+              <Text fw={600}>{album.title}</Text>
+              <Text>from this artist?</Text>
+            </Group>
+            <Text fz={'sm'} c={'dimmed'}>
+              This action will result in the removal of all related songs to this album.
+            </Text>
+          </Stack>
+        }
+        onYes={handleRemove}
+      />
+    </>
   )
 }
 
--- repertoire.ui/src/components/artist/ArtistSongCard.tsx
+++ repertoire.ui/src/components/artist/ArtistSongCard.tsx
@@ -6,7 +6,8 @@ import { useAppDispatch } from '../../state/store.ts'
 import { openAlbumDrawer, openSongDrawer } from '../../state/globalSlice.ts'
 import { MouseEvent, useState } from 'react'
 import { IconDots, IconTrash } from '@tabler/icons-react'
-import { useHover } from '@mantine/hooks'
+import {useDisclosure, useHover} from '@mantine/hooks'
+import WarningModal from '../modal/WarningModal.tsx'
 
 interface ArtistSongCardProps {
   song: Song
@@ -22,6 +23,9 @@ function ArtistSongCard({ song, handleRemove, isUnknownArtist }: ArtistSongCardP
 
   const isSelected = hovered || isMenuOpened
 
+  const [openedRemoveWarning, { open: openRemoveWarning, close: closeRemoveWarning }] =
+    useDisclosure(false)
+
   function handleClick() {
     dispatch(openSongDrawer(song.id))
   }
@@ -31,81 +35,99 @@ function ArtistSongCard({ song, handleRemove, isUnknownArtist }: ArtistSongCardP
     dispatch(openAlbumDrawer(song.album.id))
   }
 
-  function handleDelete(e: MouseEvent) {
+  function handleOpenRemoveWarning(e: MouseEvent) {
     e.stopPropagation()
-    handleRemove()
+    openRemoveWarning()
   }
 
   return (
-    <Group
-      ref={ref}
-      align={'center'}
-      wrap={'nowrap'}
-      sx={(theme) => ({
-        cursor: 'default',
-        transition: '0.3s',
-        ...(isSelected && {
-          boxShadow: theme.shadows.xl,
-          backgroundColor: alpha(theme.colors.cyan[0], 0.15)
-        })
-      })}
-      px={'md'}
-      py={'xs'}
-      onClick={handleClick}
-    >
-      <Avatar radius={'8px'} src={song.imageUrl ?? songPlaceholder} />
+    <>
+      <Group
+        ref={ref}
+        align={'center'}
+        wrap={'nowrap'}
+        sx={(theme) => ({
+          cursor: 'default',
+          transition: '0.3s',
+          ...(isSelected && {
+            boxShadow: theme.shadows.xl,
+            backgroundColor: alpha(theme.colors.cyan[0], 0.15)
+          })
+        })}
+        px={'md'}
+        py={'xs'}
+        onClick={handleClick}
+      >
+        <Avatar radius={'8px'} src={song.imageUrl ?? songPlaceholder} />
 
-      <Stack gap={0} flex={1} style={{ overflow: 'hidden' }}>
-        <Group gap={4}>
-          <Text fw={500} truncate={'end'}>
-            {song.title}
-          </Text>
-          {song.album && (
-            <>
-              <Text fz={'sm'}>-</Text>
-              <Text
-                fz={'sm'}
-                c={'dimmed'}
-                truncate={'end'}
-                sx={{ '&:hover': { textDecoration: 'underline' } }}
-                onClick={handleAlbumClick}
-              >
-                {song.album.title}
-              </Text>
-            </>
+        <Stack gap={0} flex={1} style={{ overflow: 'hidden' }}>
+          <Group gap={4}>
+            <Text fw={500} truncate={'end'}>
+              {song.title}
+            </Text>
+            {song.album && (
+              <>
+                <Text fz={'sm'}>-</Text>
+                <Text
+                  fz={'sm'}
+                  c={'dimmed'}
+                  truncate={'end'}
+                  sx={{ '&:hover': { textDecoration: 'underline' } }}
+                  onClick={handleAlbumClick}
+                >
+                  {song.album.title}
+                </Text>
+              </>
+            )}
+          </Group>
+          {song.releaseDate && (
+            <Text fz={'xs'} c={'dimmed'}>
+              {dayjs(song.releaseDate).format('DD MMM YYYY')}
+            </Text>
           )}
-        </Group>
-        {song.releaseDate && (
-          <Text fz={'xs'} c={'dimmed'}>
-            {dayjs(song.releaseDate).format('DD MMM YYYY')}
-          </Text>
-        )}
-      </Stack>
+        </Stack>
 
-      <Menu position={'bottom-end'} opened={isMenuOpened} onChange={setIsMenuOpened}>
-        <Menu.Target>
-          <ActionIcon
-            size={'md'}
-            variant={'grey'}
-            onClick={(e) => e.stopPropagation()}
-            style={{
-              transition: '0.3s',
-              opacity: isSelected ? 1 : 0
-            }}
-          >
-            <IconDots size={15} />
-          </ActionIcon>
-        </Menu.Target>
+        <Menu position={'bottom-end'} opened={isMenuOpened} onChange={setIsMenuOpened}>
+          <Menu.Target>
+            <ActionIcon
+              size={'md'}
+              variant={'grey'}
+              onClick={(e) => e.stopPropagation()}
+              style={{
+                transition: '0.3s',
+                opacity: isSelected ? 1 : 0
+              }}
+            >
+              <IconDots size={15} />
+            </ActionIcon>
+          </Menu.Target>
 
-        <Menu.Dropdown>
-          {!isUnknownArtist && (
-            <Menu.Item leftSection={<IconTrash size={14} />} c={'red.5'} onClick={handleDelete}>
-              Remove from Artist
-            </Menu.Item>
-          )}
-        </Menu.Dropdown>
-      </Menu>
-    </Group>
+          <Menu.Dropdown>
+            {!isUnknownArtist && (
+              <Menu.Item leftSection={<IconTrash size={14} />} c={'red.5'} onClick={handleOpenRemoveWarning}>
+                Remove from Artist
+              </Menu.Item>
+            )}
+          </Menu.Dropdown>
+        </Menu>
+      </Group>
+
+      <WarningModal
+        opened={openedRemoveWarning}
+        onClose={closeRemoveWarning}
+        title={`Remove ${song.title}`}
+        description={
+          <Stack gap={4}>
+            <Group gap={4}>
+              <Text>Are you sure you want to remove</Text>
+              <Text fw={600}>{song.title}</Text>
+              <Text>from this artist?</Text>
+            </Group>
+          </Stack>
+        }
+        onYes={handleRemove}
+      />
+    </>
   )
 }
 
