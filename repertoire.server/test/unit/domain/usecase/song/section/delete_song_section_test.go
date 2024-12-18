b3a7ef7b63525e10980cc07ca7dca4e677af5b63
Cris <filipcristimihail@yahoo.com>
2024-12-18 12:31:34 +0100

    #229 Server - Domain: Delete Song Section - Update Song Stats (Confidence, Rehearsals and Progress)

--- repertoire.server/test/unit/domain/usecase/song/section/delete_song_section_test.go
+++ repertoire.server/test/unit/domain/usecase/song/section/delete_song_section_test.go
@@ -2,6 +2,7 @@ package section
 
 import (
 	"errors"
+	"math"
 	"net/http"
 	"repertoire/server/domain/usecase/song/section"
 	"repertoire/server/model"
@@ -164,50 +165,126 @@ func TestDeleteSongSection_WhenDeleteSectionFails_ShouldReturnInternalServerErro
 }
 
 func TestDeleteSongSection_WhenSuccessful_ShouldNotReturnAnyError(t *testing.T) {
-	// given
-	songRepository := new(repository.SongRepositoryMock)
-	_uut := section.NewDeleteSongSection(songRepository)
-
-	id := uuid.New()
-	songID := uuid.New()
-
-	// given - mocking
-	song := &model.Song{
-		ID: songID,
-		Sections: []model.SongSection{
-			{ID: uuid.New(), Order: 0},
-			{ID: id, Order: 1},
-			{ID: uuid.New(), Order: 2},
-			{ID: uuid.New(), Order: 3},
-			{ID: uuid.New(), Order: 4},
+	tests := []struct {
+		name                   string
+		song                   model.Song
+		sectionsIndex          uint
+		expectedSongConfidence float64
+		expectedSongRehearsals float64
+		expectedSongProgress   float64
+	}{
+		{
+			"1 - When it was the only section",
+			model.Song{
+				ID: uuid.New(),
+				Sections: []model.SongSection{
+					{ID: uuid.New(), Order: 0},
+				},
+			},
+			0,
+			0,
+			0,
+			0,
+		},
+		{
+			"2 - When it was the only section with stats",
+			model.Song{
+				ID: uuid.New(),
+				Sections: []model.SongSection{
+					{ID: uuid.New(), Order: 0, Confidence: 55, Rehearsals: 25, Progress: 39},
+				},
+				Confidence: 55,
+				Rehearsals: 25,
+				Progress:   39,
+			},
+			0,
+			0,
+			0,
+			0,
+		},
+		{
+			"3 - When there are more sections, but not stats",
+			model.Song{
+				ID: uuid.New(),
+				Sections: []model.SongSection{
+					{ID: uuid.New(), Order: 0},
+					{ID: uuid.New(), Order: 1},
+					{ID: uuid.New(), Order: 2},
+					{ID: uuid.New(), Order: 3},
+					{ID: uuid.New(), Order: 4},
+				},
+			},
+			2,
+			0,
+			0,
+			0,
+		},
+		{
+			"4 - When there are more sections with stats",
+			model.Song{
+				ID: uuid.New(),
+				Sections: []model.SongSection{
+					{ID: uuid.New(), Order: 0, Confidence: 55, Rehearsals: 12, Progress: 45},
+					{ID: uuid.New(), Order: 1, Confidence: 23, Rehearsals: 5, Progress: 15},
+					{ID: uuid.New(), Order: 2, Confidence: 78, Rehearsals: 25, Progress: 100},
+					{ID: uuid.New(), Order: 3, Confidence: 40, Rehearsals: 6, Progress: 63},
+					{ID: uuid.New(), Order: 4, Confidence: 80, Rehearsals: 19, Progress: 170},
+				},
+				Confidence: 55.2,
+				Rehearsals: 13.4,
+				Progress:   78.6,
+			},
+			2,
+			50,
+			11,
+			73,
 		},
 	}
-	songRepository.On("GetWithSections", new(model.Song), songID).
-		Return(nil, song).
-		Once()
 
-	songRepository.On("UpdateWithAssociations", mock.IsType(song)).
-		Run(func(args mock.Arguments) {
-			newSong := args.Get(0).(*model.Song)
-			sections := slices.Clone(newSong.Sections)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			songRepository := new(repository.SongRepositoryMock)
+			_uut := section.NewDeleteSongSection(songRepository)
 
-			sections = slices.DeleteFunc(sections, func(a model.SongSection) bool {
-				return a.ID == id
-			})
-			for i, s := range sections {
-				assert.Equal(t, uint(i), s.Order)
-			}
-		}).
-		Return(nil).
-		Once()
+			id := tt.song.Sections[tt.sectionsIndex].ID
+			songID := tt.song.ID
 
-	songRepository.On("DeleteSection", id).Return(nil).Once()
+			// given - mocking
+			songRepository.On("GetWithSections", new(model.Song), songID).
+				Return(nil, &tt.song).
+				Once()
 
-	// when
-	errCode := _uut.Handle(id, songID)
+			songRepository.On("UpdateWithAssociations", mock.IsType(&tt.song)).
+				Run(func(args mock.Arguments) {
+					newSong := args.Get(0).(*model.Song)
 
-	// then
-	assert.Nil(t, errCode)
+					// stats updated
+					assert.Equal(t, tt.expectedSongConfidence, math.Round(newSong.Confidence))
+					assert.Equal(t, tt.expectedSongRehearsals, math.Round(newSong.Rehearsals))
+					assert.Equal(t, tt.expectedSongProgress, math.Round(newSong.Progress))
 
-	songRepository.AssertExpectations(t)
+					// sections ordered
+					sections := slices.Clone(newSong.Sections)
+					sections = slices.DeleteFunc(sections, func(a model.SongSection) bool {
+						return a.ID == id
+					})
+					for i, s := range sections {
+						assert.Equal(t, uint(i), s.Order)
+					}
+				}).
+				Return(nil).
+				Once()
+
+			songRepository.On("DeleteSection", id).Return(nil).Once()
+
+			// when
+			errCode := _uut.Handle(id, songID)
+
+			// then
+			assert.Nil(t, errCode)
+
+			songRepository.AssertExpectations(t)
+		})
+	}
 }
