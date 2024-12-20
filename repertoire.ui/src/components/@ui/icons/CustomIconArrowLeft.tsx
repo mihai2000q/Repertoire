2c24fcf7e5b9e8c23e8b43557a0900b49d78003b
Cris <filipcristimihail@yahoo.com>
2024-12-20 12:41:38 +0100

    #239 UI - Update Custom Icons Arrow Left and Right to forward ref for tooltip

--- repertoire.ui/src/components/@ui/icons/CustomIconArrowLeft.tsx
+++ repertoire.ui/src/components/@ui/icons/CustomIconArrowLeft.tsx
@@ -1,16 +1,19 @@
-interface CustomIconArrowLeftProps {
+import { forwardRef, SVGProps } from 'react'
+
+interface CustomIconArrowLeftProps extends SVGProps<SVGSVGElement> {
   color?: string
   size?: number
 }
 
-function CustomIconArrowLeft({ color, size = 24 }: CustomIconArrowLeftProps) {
-  return (
+const CustomIconArrowLeft = forwardRef<SVGSVGElement, CustomIconArrowLeftProps>(
+  ({ color, size = 24 }, ref) => (
     <svg
+      ref={ref}
       xmlns="http://www.w3.org/2000/svg"
       width={size}
       height={size}
-      fill={color || "currentColor"}
-      stroke={color || "currentColor"}
+      fill={color || 'currentColor'}
+      stroke={color || 'currentColor'}
       strokeLinecap="round"
       strokeLinejoin="round"
       viewBox="0 0 1024 1024"
@@ -18,6 +21,8 @@ function CustomIconArrowLeft({ color, size = 24 }: CustomIconArrowLeftProps) {
       <path d="M604.7 759.2l61.8-61.8L481.1 512l185.4-185.4-61.8-61.8L357.5 512z" />
     </svg>
   )
-}
+)
+
+CustomIconArrowLeft.displayName = "CustomIconArrowLeft";
 
 export default CustomIconArrowLeft
