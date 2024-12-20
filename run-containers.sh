20ba44fa2156f807e138f755341e267c70354ba9
Cris <filipcristimihail@yahoo.com>
2024-12-20 15:50:42 +0100

    #243 Run Containers Shell Script - Apply migration after running docker

--- run-containers.sh
+++ run-containers.sh
@@ -1,5 +1,6 @@
 DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker-compose -f ${DIR_SCRIPT}/repertoire.server/docker-compose.yaml up -d
+sh ${DIR_SCRIPT}/repertoire.server/apply-migrations.sh
 docker-compose -f ${DIR_SCRIPT}/repertoire.storage/docker-compose.yaml up -d
 docker-compose -f ${DIR_SCRIPT}/repertoire.web/docker-compose.yaml up -d
