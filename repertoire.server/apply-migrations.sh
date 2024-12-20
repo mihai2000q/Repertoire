53fe04b4eabd81ec1d467743348af4a549135ae5
Cris <filipcristimihail@yahoo.com>
2024-12-20 16:04:45 +0100

    #243 Server - Apply Migration Shell Script - MIGRATION DIR with prefix path

--- repertoire.server/apply-migrations.sh
+++ repertoire.server/apply-migrations.sh
@@ -1,10 +1,12 @@
-DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIRECTORY_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source ${DIR_SCRIPT}/.env
+echo ${DIRECTORY_SCRIPT}
+
+source ${DIRECTORY_SCRIPT}/.env
 
 export GOOSE_DRIVER=postgres
 export GOOSE_DBSTRING=postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
-export GOOSE_MIGRATION_DIR=migrations/
+export GOOSE_MIGRATION_DIR=${DIRECTORY_SCRIPT}/migrations/
 
 goose up
 
