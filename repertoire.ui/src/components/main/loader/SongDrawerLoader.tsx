bd8f05ddd212de5f758675fecf0c392797ee6c83
Cris <filipcristimihail@yahoo.com>
2024-12-22 15:55:50 +0100

    #250 UI - Main - Redo Song Drawer Loader

--- repertoire.ui/src/components/main/loader/SongDrawerLoader.tsx
+++ repertoire.ui/src/components/main/loader/SongDrawerLoader.tsx
@@ -1,17 +1,77 @@
-import { Skeleton, Stack } from '@mantine/core'
+import {Divider, Grid, Group, Skeleton, Stack} from '@mantine/core'
 
 function SongDrawerLoader() {
   return (
     <Stack gap={'xs'} data-testid={'song-drawer-loader'}>
-      <Skeleton radius={0} w={'100%'} h={400} />
+      <Skeleton radius={0} w={'100%'} h={300} />
 
-      <Stack gap={'xs'} px={'md'}>
-        <Skeleton w={300} h={25} />
-        <Stack gap={2}>
-          <Skeleton w={400} h={8} />
-          <Skeleton w={400} h={8} />
+      <Stack px={'md'} pb={'xs'} gap={4}>
+        <Skeleton w={220} h={25} />
+        <Group gap={4}>
+          <Skeleton radius={'50%'} w={35} h={35} />
+          <Skeleton w={80} h={15} />
+          <Skeleton w={110} h={15} />
+          <Skeleton w={45} h={15} />
+        </Group>
+
+        <Stack gap={4} my={'xs'} px={'xs'}>
+          <Skeleton w={'100%'} h={8} />
+          <Skeleton w={'100%'} h={8} />
           <Skeleton w={50} h={8} />
         </Stack>
+
+        <Divider />
+
+        <Grid gutter={'md'} p={'xs'}>
+          <Grid.Col span={4}>
+            <Skeleton w={100} h={12} />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Skeleton w={175} h={15} />
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <Skeleton w={50} h={12} />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Skeleton w={70} h={15} />
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <Skeleton w={75} h={12} />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Skeleton w={90} h={15} />
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <Skeleton w={60} h={12} />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Skeleton w={35} h={15} />
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <Skeleton w={80} h={12} />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Skeleton w={200} h={15} />
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <Skeleton w={75} h={12} />
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Skeleton w={200} h={15} />
+          </Grid.Col>
+        </Grid>
+
+        <Divider my={4} />
+
+        <Group gap={'xs'} style={{ alignSelf: 'flex-end' }}>
+          <Skeleton radius={'sm'} w={23} h={23} />
+          <Skeleton radius={'sm'} w={23} h={23} />
+        </Group>
       </Stack>
     </Stack>
   )
