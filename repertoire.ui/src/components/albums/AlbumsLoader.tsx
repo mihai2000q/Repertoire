78c536576c7c3c9e99f5a3d9c41e345b55886a21
Cris <filipcristimihail@yahoo.com>
2024-12-22 13:39:25 +0100

    #250 UI - Albums - Cleanup Loader + XL Gap

--- repertoire.ui/src/components/albums/AlbumsLoader.tsx
+++ repertoire.ui/src/components/albums/AlbumsLoader.tsx
@@ -2,10 +2,15 @@ import { Group, Skeleton, Stack } from '@mantine/core'
 
 function AlbumsLoader() {
   return (
-    <Group data-testid="albums-loader">
+    <Group gap={'xl'} data-testid="albums-loader">
       {Array.from(Array(20)).map((_, i) => (
         <Stack key={i} gap={'xs'} align={'center'}>
-          <Skeleton radius={'lg'} h={150} w={150} style={(theme) => ({ boxShadow: theme.shadows.sm })} />
+          <Skeleton
+            radius={'lg'}
+            h={150}
+            w={150}
+            style={(theme) => ({ boxShadow: theme.shadows.sm })}
+          />
           <Stack gap={0} align={'center'}>
             <Skeleton w={100} h={15} mb={4} />
             <Skeleton w={60} h={10} />
