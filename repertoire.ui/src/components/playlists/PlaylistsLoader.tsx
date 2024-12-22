eb4c5b5b30f1183a041f548987840fe8ecf7cec9
Cris <filipcristimihail@yahoo.com>
2024-12-22 13:53:07 +0100

    #250 UI - Playlists - Redo Loader

--- repertoire.ui/src/components/playlists/PlaylistsLoader.tsx
+++ repertoire.ui/src/components/playlists/PlaylistsLoader.tsx
@@ -1,24 +1,18 @@
-import { Card, Group, Skeleton, Stack } from '@mantine/core'
+import { Group, Skeleton, Stack } from '@mantine/core'
 
 function PlaylistsLoader() {
   return (
     <Group data-testid="playlists-loader">
       {Array.from(Array(20)).map((_, i) => (
-        <Card key={i} p="sm" shadow="md" h={253} w={175}>
-          <Card.Section>
-            <Skeleton h={150} />
-          </Card.Section>
-
-          <Group justify="space-between" mt="sm" mb="xs">
-            <Skeleton w={80} h={12} />
-          </Group>
-
-          <Stack gap={4}>
-            <Skeleton w={150} h={8} />
-            <Skeleton w={150} h={8} />
-            <Skeleton w={80} h={8} />
-          </Stack>
-        </Card>
+        <Stack key={i} gap={'xs'} align={'center'}>
+          <Skeleton
+            radius={'lg'}
+            h={150}
+            w={150}
+            style={(theme) => ({ boxShadow: theme.shadows.sm })}
+          />
+          <Skeleton w={100} h={15} mb={4} />
+        </Stack>
       ))}
     </Group>
   )
