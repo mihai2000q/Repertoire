b83e5c506c24c067336badaafe930d66c89c1919
Cris <filipcristimihail@yahoo.com>
2024-12-23 19:08:29 +0100

    #247 UI - Home View - only home

--- repertoire.ui/src/views/Home.tsx
+++ repertoire.ui/src/views/Home.tsx
@@ -3,8 +3,7 @@ import { ReactElement } from 'react'
 function Home(): ReactElement {
   return (
     <div>
-      <h1>Home View</h1>
-      <div>This is the home view</div>
+      <h1>Home</h1>
     </div>
   )
 }
