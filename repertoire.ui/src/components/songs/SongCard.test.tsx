dcbd49d746d9f8b28cfb697d57b27d00143b16e8
Cris <filipcristimihail@yahoo.com>
2025-01-09 12:38:41 +0100

    #253 UI - Songs - Song Card - Refactor unit test

--- repertoire.ui/src/components/songs/SongCard.test.tsx
+++ repertoire.ui/src/components/songs/SongCard.test.tsx
@@ -1,52 +1,283 @@
 import { screen } from '@testing-library/react'
 import SongCard from './SongCard'
 import Song from '../../types/models/Song'
-import { reduxRouterRender } from '../../test-utils'
+import { reduxRouterRender, withToastify } from '../../test-utils'
+import Artist from '../../types/models/Artist.ts'
+import { userEvent } from '@testing-library/user-event'
+import Difficulty from '../../utils/enums/Difficulty.ts'
+import { RootState } from '../../state/store.ts'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
 
 describe('Song Card', () => {
-  it('should render and display minimal info', ({ expect }) => {
-    // Arrange
-    const song: Song = {
-      id: '',
-      title: 'Some song',
-      description: '',
-      isRecorded: false,
-      sections: [],
-      rehearsals: 0,
-      confidence: 0,
-      progress: 0,
-      createdAt: '',
-      updatedAt: ''
-    }
+  const song: Song = {
+    id: '',
+    title: 'Some song',
+    description: '',
+    isRecorded: false,
+    sections: [],
+    rehearsals: 0,
+    confidence: 0,
+    progress: 0,
+    createdAt: '',
+    updatedAt: ''
+  }
 
-    // Act
-    const [{ container }] = reduxRouterRender(<SongCard song={song} />)
+  const artist: Artist = {
+    id: '1',
+    name: 'Artist 1',
+    createdAt: '',
+    updatedAt: '',
+    albums: [],
+    songs: []
+  }
 
-    // Assert
-    expect(screen.getByText(song.title)).toBeVisible()
-    expect(container.querySelector('svg.tabler-icon-microphone-filled')).not.toBeInTheDocument()
+  const server = setupServer()
+
+  beforeAll(() => server.listen())
+
+  afterEach(() => server.resetHandlers())
+
+  afterAll(() => server.close())
+
+  it('should render and display minimal info', () => {
+    reduxRouterRender(<SongCard song={song} />)
+
+    expect(screen.getByText(song.title)).toBeInTheDocument()
+    expect(screen.getByText(/unknown/i)).toBeInTheDocument()
   })
 
-  it('should render and display maximal info', ({ expect }) => {
+  it('should render and display icons when the song is recorded, has guitar tuning, and songsterr and youtube links', async () => {
     // Arrange
-    const song: Song = {
-      id: '',
-      title: 'Some song',
-      description: '',
+    const user = userEvent.setup()
+
+    const localSong: Song = {
+      ...song,
+      artist: artist,
       isRecorded: true,
-      sections: [],
-      rehearsals: 0,
-      confidence: 0,
-      progress: 0,
-      createdAt: '',
-      updatedAt: ''
+      guitarTuning: {
+        id: '',
+        name: 'Drop D'
+      },
+      difficulty: Difficulty.Impossible,
+      songsterrLink: 'this is a link',
+      youtubeLink: 'this is a link'
     }
 
     // Act
-    const [{ container }] = reduxRouterRender(<SongCard song={song} />)
+    reduxRouterRender(<SongCard song={localSong} />)
 
     // Assert
-    expect(screen.getByText(song.title)).toBeVisible()
-    expect(container.querySelector('svg.tabler-icon-microphone-filled')).toBeVisible()
+    expect(screen.getByText(localSong.title)).toBeInTheDocument()
+    expect(screen.getByText(localSong.artist.name)).toBeInTheDocument()
+    expect(screen.getByLabelText('recorded-icon')).toBeInTheDocument()
+    expect(screen.getByLabelText('guitar-tuning-icon')).toBeInTheDocument()
+    expect(screen.getByLabelText('difficulty-icon')).toBeInTheDocument()
+    expect(screen.getByLabelText('songsterr-icon')).toBeInTheDocument()
+    expect(screen.getByLabelText('youtube-icon')).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('recorded-icon'))
+    expect(await screen.findByText(/is recorded/i)).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('guitar-tuning-icon'))
+    expect(await screen.findByText(new RegExp(localSong.guitarTuning.name))).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('difficulty-icon'))
+    expect(await screen.findByText(new RegExp(localSong.difficulty))).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('songsterr-icon'))
+    expect(await screen.findByText(/songsterr/i)).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('youtube-icon'))
+    expect(await screen.findByText(/youtube/i)).toBeInTheDocument()
+  })
+
+  it('should render and display solo icon when the song has exactly one Solo section', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    const localSong: Song = {
+      ...song,
+      sections: [
+        {
+          id: '',
+          name: 'Solo 1',
+          songSectionType: {
+            id: '',
+            name: 'Solo'
+          },
+          rehearsals: 0,
+          confidence: 0,
+          progress: 0
+        }
+      ]
+    }
+
+    // Act
+    reduxRouterRender(<SongCard song={localSong} />)
+
+    // Assert
+    expect(screen.getByLabelText('solo-icon')).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('solo-icon'))
+    expect(await screen.findByText(/a solo/i)).toBeInTheDocument()
+  })
+
+  it('should render and display solos icon when the song has more than one Solo section', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    const localSong: Song = {
+      ...song,
+      sections: [
+        {
+          id: '',
+          name: 'Solo 1',
+          songSectionType: {
+            id: '',
+            name: 'Solo'
+          },
+          rehearsals: 0,
+          confidence: 0,
+          progress: 0
+        },
+        {
+          id: '',
+          name: 'Solo 2',
+          songSectionType: {
+            id: '',
+            name: 'Solo'
+          },
+          rehearsals: 0,
+          confidence: 0,
+          progress: 0
+        }
+      ]
+    }
+
+    // Act
+    reduxRouterRender(<SongCard song={localSong} />)
+
+    // Assert
+    expect(screen.getByLabelText('solos-icon')).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('solos-icon'))
+    expect(await screen.findByText(/2 solos/i)).toBeInTheDocument()
+  })
+
+  it('should render and display riffs icon when the song has Riff sections', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    const localSong: Song = {
+      ...song,
+      sections: [
+        {
+          id: '',
+          name: 'Riff 1',
+          songSectionType: {
+            id: '',
+            name: 'Riff'
+          },
+          rehearsals: 0,
+          confidence: 0,
+          progress: 0
+        },
+        {
+          id: '',
+          name: 'Riff 2',
+          songSectionType: {
+            id: '',
+            name: 'Riff'
+          },
+          rehearsals: 0,
+          confidence: 0,
+          progress: 0
+        }
+      ]
+    }
+
+    // Act
+    reduxRouterRender(<SongCard song={localSong} />)
+
+    // Assert
+    expect(screen.getByLabelText('riffs-icon')).toBeInTheDocument()
+
+    await user.hover(screen.getByLabelText('riffs-icon'))
+    expect(await screen.findByText(/2 riffs/i)).toBeInTheDocument()
+  })
+
+  it('should display menu on right click', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    // Act
+    reduxRouterRender(<SongCard song={song} />)
+
+    // Assert
+    await user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByRole('img', { name: song.title })
+    })
+    expect(screen.getByRole('menuitem', { name: /delete/i })).toBeInTheDocument()
+  })
+
+  describe('on menu', () => {
+    it('should display warning modal when clicking on delete', async () => {
+      // Arrange
+      const user = userEvent.setup()
+
+      server.use(
+        http.delete(`/songs/${song.id}`, async () => {
+          return HttpResponse.json({ message: 'it worked' })
+        })
+      )
+
+      // Act
+      reduxRouterRender(withToastify(<SongCard song={song} />))
+
+      // Assert
+      await user.pointer({
+        keys: '[MouseRight>]',
+        target: screen.getByRole('img', { name: song.title })
+      })
+      await user.click(screen.getByRole('menuitem', { name: /delete/i }))
+
+      expect(screen.getByRole('heading', { name: /delete/i })).toBeInTheDocument()
+      await user.click(screen.getByRole('button', { name: /yes/i }))
+
+      expect(screen.getByText(`${song.title} deleted!`)).toBeInTheDocument()
+    })
+  })
+
+  it('should navigate on click', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    // Act
+    reduxRouterRender(<SongCard song={song} />)
+
+    // Assert
+    await user.click(screen.getByRole('img', { name: song.title }))
+    expect(window.location.pathname).toBe(`/song/${song.id}`)
+  })
+
+  it('should open artist drawer on artist name click', async () => {
+    // Arrange
+    const user = userEvent.setup()
+
+    const localSong = {
+      ...song,
+      artist: artist
+    }
+
+    // Act
+    const [_, store] = reduxRouterRender(<SongCard song={localSong} />)
+
+    // Assert
+    await user.click(screen.getByText(localSong.artist.name))
+
+    expect((store.getState() as RootState).global.artistDrawer.open).toBeTruthy()
+    expect((store.getState() as RootState).global.artistDrawer.artistId).toBe(localSong.artist.id)
   })
 })
