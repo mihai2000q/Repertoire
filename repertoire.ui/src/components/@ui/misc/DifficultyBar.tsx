82e7dcafaa104a97be43bb2e6b1f0707c5b5fbbf
Cris <filipcristimihail@yahoo.com>
2025-01-11 22:35:25 +0100

    #253 UI - Misc Components - Add Unit Test for Difficulty Bar

--- repertoire.ui/src/components/@ui/misc/DifficultyBar.tsx
+++ repertoire.ui/src/components/@ui/misc/DifficultyBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Group, Progress, Tooltip } from '@mantine/core'
+import { Group, Progress, Tooltip } from '@mantine/core'
 import Difficulty from '../../../utils/enums/Difficulty.ts'
 import useDifficultyInfo from '../../../hooks/useDifficultyInfo.ts'
 
@@ -10,21 +10,19 @@ function DifficultyBar({ difficulty }: DifficultyBarProps) {
   const { number: difficultyNumber, color: difficultyColor } = useDifficultyInfo(difficulty)
 
   return (
-    <Box role={'progressbar'} aria-label={'difficulty'}>
-      <Tooltip label={`This song is ${difficulty}`} openDelay={400} position={'top'}>
-        <Group grow gap={4}>
-          {Array.from(Array(Object.entries(Difficulty).length)).map((_, i) => (
-            <Progress
-              key={i}
-              size={5}
-              maw={40}
-              value={i + 1 <= difficultyNumber ? 100 : 0}
-              color={difficultyColor}
-            />
-          ))}
-        </Group>
-      </Tooltip>
-    </Box>
+    <Tooltip label={`This song is ${difficulty}`} openDelay={400} position={'top'}>
+      <Group grow gap={4} role={'progressbar'} aria-label={'difficulty'}>
+        {Array.from(Array(Object.entries(Difficulty).length)).map((_, i) => (
+          <Progress
+            key={i}
+            size={5}
+            maw={40}
+            value={i + 1 <= difficultyNumber ? 100 : 0}
+            color={difficultyColor}
+          />
+        ))}
+      </Group>
+    </Tooltip>
   )
 }
 
