08d963e3f68675c4358bd3c187219664d0635db6
Cris <filipcristimihail@yahoo.com>
2025-01-27 00:28:49 +0100

    #294 UI - Add Enum: Song Property

---
+++ repertoire.ui/src/utils/enums/SongProperty.ts
@@ -0,0 +1,11 @@
+enum SongProperty {
+  AlbumTrackNo = 'album_track_no',
+  Title = 'title',
+  Difficulty = 'difficulty',
+  Rehearsals = 'rehearsals',
+  Confidence = 'confidence',
+  Progress = 'progress',
+  LastTimePlayed = 'last_time_played',
+}
+
+export default SongProperty
