3db8aacc6e4e77ed05c5ab9848fc0f5bdea2a67c
Cris <filipcristimihail@yahoo.com>
2025-01-27 17:04:44 +0100

    #296 UI - Artist - Add More Song Orders: Difficulty, Rehearsals, Confidence, Progress, Last Time Played

--- repertoire.ui/src/data/artist/artistSongsOrders.ts
+++ repertoire.ui/src/data/artist/artistSongsOrders.ts
@@ -1,9 +1,26 @@
 import Order from '../../types/Order.ts'
+import SongProperty from '../../utils/enums/SongProperty.ts'
 
 const artistSongsOrders: Order[] = [
-  { value: 'release_date desc, title asc', label: 'Latest Releases' },
-  { value: 'title asc', label: 'Alphabetically' },
-  { value: 'release_date asc, title asc', label: 'First Releases' }
+  {
+    value: 'release_date desc nulls last, title asc',
+    label: 'Latest Releases',
+    property: SongProperty.ReleaseDate
+  },
+  { value: 'title asc', label: 'Alphabetically', property: SongProperty.Title },
+  {
+    value: 'difficulty desc nulls last, title asc',
+    label: 'Difficulty',
+    property: SongProperty.Difficulty
+  },
+  { value: 'rehearsals desc, title asc', label: 'Rehearsals', property: SongProperty.Rehearsals },
+  { value: 'confidence desc, title asc', label: 'Confidence', property: SongProperty.Confidence },
+  { value: 'progress desc, title asc', label: 'Progress', property: SongProperty.Progress },
+  {
+    value: 'last_time_played desc nulls last, title asc',
+    label: 'Last Time Played',
+    property: SongProperty.LastTimePlayed
+  }
 ]
 
 export default artistSongsOrders
