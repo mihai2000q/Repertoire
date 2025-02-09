9b8dce90fdb4d9c4b45966b3d0753d6d55e7f897
Cris <filipcristimihail@yahoo.com>
2025-02-10 00:10:04 +0100

    #343 UI - New Custom Icons: Acoustic Guitar, Bass, Drums, Flute, Harp, Piano, Piano Keyboard, Saxophone, Ukulele, Violin, Voice + Rename from Flying V to King V and Flying V2 to Rhoads and Electric Guitar suffix with Warlock + Remove ST Electric Guitar Icon

--- repertoire.ui/src/components/@ui/icons/CustomIcons.test.tsx
+++ repertoire.ui/src/components/@ui/icons/CustomIcons.test.tsx
@@ -1,15 +1,23 @@
 import { render } from '@testing-library/react'
 import CustomIconArrowLeft from './CustomIconArrowLeft.tsx'
 import CustomIconArrowRight from './CustomIconArrowRight.tsx'
-import CustomIconElectricGuitar from './CustomIconElectricGuitar.tsx'
-import CustomIconElectricGuitarST from './CustomIconElectricGuitarST.tsx'
-import CustomIconFlyingVGuitar from './CustomIconFlyingVGuitar.tsx'
-import CustomIconFlyingV2Guitar from './CustomIconFlyingV2Guitar.tsx'
+import CustomIconElectricGuitarWarlock from './CustomIconElectricGuitarWarlock.tsx'
+import CustomIconKingVGuitar from './CustomIconKingVGuitar.tsx'
+import CustomIconRhoadsGuitar from './CustomIconRhoadsGuitar.tsx'
 import CustomIconGuitarHead from './CustomIconGuitarHead.tsx'
 import CustomIconLesPaulGuitar from './CustomIconLesPaulGuitar.tsx'
 import CustomIconLesPaulGuitarOutlined from './CustomIconLesPaulGuitarOutlined.tsx'
 import CustomIconMetronome from './CustomIconMetronome.tsx'
 import CustomIconLightningTrio from './CustomIconLightningTrio.tsx'
+import CustomIconAcousticGuitar from './CustomIconAcousticGuitar.tsx'
+import CustomIconFlute from './CustomIconFlute.tsx'
+import CustomIconHarp from './CustomIconHarp.tsx'
+import CustomIconPiano from './CustomIconPiano.tsx'
+import CustomIconPianoKeyboard from './CustomIconPianoKeyboard.tsx'
+import CustomIconSaxophone from './CustomIconSaxophone.tsx'
+import CustomIconUkulele from './CustomIconUkulele.tsx'
+import CustomIconViolin from './CustomIconViolin.tsx'
+import CustomIconVoice from './CustomIconVoice.tsx'
 
 describe.concurrent('Custom Icons', () => {
   it('should render Custom Icon Arrow Left', () => {
@@ -20,24 +28,28 @@ describe.concurrent('Custom Icons', () => {
     render(<CustomIconArrowRight />)
   })
 
-  it('should render Custom Icon Electric Guitar', () => {
-    render(<CustomIconElectricGuitar />)
+  it('should render Custom Icon Acoustic Guitar', () => {
+    render(<CustomIconAcousticGuitar />)
   })
 
-  it('should render Custom Icon Electric Guitar ST', () => {
-    render(<CustomIconElectricGuitarST />)
+  it('should render Custom Icon Electric Guitar Warlock', () => {
+    render(<CustomIconElectricGuitarWarlock />)
   })
 
-  it('should render Custom Icon Flying V2 Guitar', () => {
-    render(<CustomIconFlyingV2Guitar />)
-  })
-
-  it('should render Custom Icon Flying V Guitar', () => {
-    render(<CustomIconFlyingVGuitar />)
+  it('should render Custom Icon Flute', () => {
+    render(<CustomIconFlute />)
   })
 
   it('should render Custom Icon Guitar Head', () => {
     render(<CustomIconGuitarHead />)
+  })
+
+  it('should render Custom Icon Harp', () => {
+    render(<CustomIconHarp />)
+  })
+
+  it('should render Custom Icon King V Guitar', () => {
+    render(<CustomIconKingVGuitar />)
   })
 
   it('should render Custom Icon Les Paul Guitar', () => {
@@ -54,5 +66,33 @@ describe.concurrent('Custom Icons', () => {
 
   it('should render Custom Icon Metronome', () => {
     render(<CustomIconMetronome />)
+  })
+
+  it('should render Custom Icon Piano', () => {
+    render(<CustomIconPiano />)
+  })
+
+  it('should render Custom Icon Piano Keyboard', () => {
+    render(<CustomIconPianoKeyboard />)
+  })
+
+  it('should render Custom Icon Rhoads Guitar', () => {
+    render(<CustomIconRhoadsGuitar />)
+  })
+
+  it('should render Custom Icon Saxophone', () => {
+    render(<CustomIconSaxophone />)
+  })
+
+  it('should render Custom Icon Ukulele', () => {
+    render(<CustomIconUkulele />)
+  })
+
+  it('should render Custom Icon Violin', () => {
+    render(<CustomIconViolin />)
+  })
+
+  it('should render Custom Icon Voice', () => {
+    render(<CustomIconVoice />)
   })
 })
