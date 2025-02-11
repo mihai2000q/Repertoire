60bf2711fc632a029d5d73e4606041baee50cf28
Cris <filipcristimihail@yahoo.com>
2025-02-11 14:14:42 +0100

    #362 UI - Topbar - Extract Search and User to their own components and unit tests

---
+++ repertoire.ui/src/components/main/TopbarUser.test.tsx
@@ -0,0 +1,81 @@
+import User from '../../types/models/User.ts'
+import { emptyUser, reduxRender } from '../../test-utils.tsx'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+import { userEvent } from '@testing-library/user-event'
+import { screen } from '@testing-library/react'
+import { RootState } from '../../state/store.ts'
+import TopbarUser from './TopbarUser.tsx'
+
+describe('Topbar User', () => {
+  const user: User = {
+    ...emptyUser,
+    id: '1',
+    email: 'Gigi@yahoo.com',
+    name: 'Gigi'
+  }
+
+  const handlers = [
+    http.get('/users/current', async () => {
+      return HttpResponse.json(user)
+    })
+  ]
+
+  const server = setupServer(...handlers)
+
+  beforeAll(() => server.listen())
+
+  afterEach(() => server.resetHandlers())
+
+  afterAll(() => server.close())
+
+  it('should display menu when clicking on the user button', async () => {
+    const userEventDispatcher = userEvent.setup()
+
+    reduxRender(<TopbarUser />)
+
+    const userButton = await screen.findByRole('button', { name: 'user' })
+    await userEventDispatcher.click(userButton)
+
+    expect(screen.getByText(user.email)).toBeInTheDocument()
+    expect(screen.getByText(user.name)).toBeInTheDocument()
+    expect(screen.getByRole('menuitem', { name: /settings/i })).toBeInTheDocument()
+    expect(screen.getByRole('menuitem', { name: /account/i })).toBeInTheDocument()
+    expect(screen.getByRole('menuitem', { name: /sign out/i })).toBeInTheDocument()
+  })
+
+  describe('on menu', () => {
+    it('should display account modal when clicking on account', async () => {
+      const userEventDispatcher = userEvent.setup()
+
+      reduxRender(<TopbarUser />)
+
+      await userEventDispatcher.click(await screen.findByRole('button', { name: 'user' }))
+      await userEventDispatcher.click(screen.getByRole('menuitem', { name: /account/i }))
+
+      expect(await screen.findByRole('dialog', { name: /account/i })).toBeInTheDocument()
+    })
+
+    it('should display settings modal when clicking on settings', async () => {
+      const userEventDispatcher = userEvent.setup()
+
+      reduxRender(<TopbarUser />)
+
+      await userEventDispatcher.click(await screen.findByRole('button', { name: 'user' }))
+      await userEventDispatcher.click(screen.getByRole('menuitem', { name: /settings/i }))
+
+      expect(await screen.findByRole('dialog', { name: /settings/i })).toBeInTheDocument()
+    })
+
+    it('should sign out when clicking on sign out', async () => {
+      const userEventDispatcher = userEvent.setup()
+
+      const [_, store] = reduxRender(<TopbarUser />)
+
+      await userEventDispatcher.click(await screen.findByRole('button', { name: 'user' }))
+      await userEventDispatcher.click(screen.getByRole('menuitem', { name: /sign out/i }))
+
+      expect((store.getState() as RootState).auth.token).toBeNull()
+    })
+  })
+})
---
+++ repertoire.ui/src/components/main/TopbarUser.tsx
@@ -0,0 +1,87 @@
+import { alpha, Avatar, Group, Loader, Menu, Stack, Text, UnstyledButton } from '@mantine/core'
+import userPlaceholder from '../../assets/user-placeholder.jpg'
+import { IconCaretDownFilled, IconLogout2, IconSettings, IconUser } from '@tabler/icons-react'
+import AccountModal from './modal/AccountModal.tsx'
+import SettingsModal from './modal/SettingsModal.tsx'
+import { useDisclosure } from '@mantine/hooks'
+import { signOut } from '../../state/slice/authSlice.ts'
+import { useGetCurrentUserQuery } from '../../state/api.ts'
+import useAuth from '../../hooks/useAuth.ts'
+import { useAppDispatch } from '../../state/store.ts'
+
+function TopbarUser() {
+  const dispatch = useAppDispatch()
+
+  const { data: user } = useGetCurrentUserQuery(undefined, {
+    skip: !useAuth()
+  })
+
+  const [openedAccount, { open: openAccount, close: closeAccount }] = useDisclosure(false)
+  const [openedSettings, { open: openSettings, close: closeSettings }] = useDisclosure(false)
+
+  function handleSignOut() {
+    dispatch(signOut())
+  }
+
+  if (!user) return <Loader size={'sm'} />
+
+  return (
+    <Menu shadow={'lg'} width={200}>
+      <Menu.Target>
+        <UnstyledButton
+          p={'4px'}
+          aria-label={'user'}
+          sx={(theme) => ({
+            borderRadius: '16px',
+            cursor: 'pointer',
+            transition: '0.175s all, transform 200ms ease-in-out',
+            color: theme.colors.gray[7],
+            '&:hover': {
+              boxShadow: theme.shadows.sm,
+              color: theme.colors.gray[8],
+              backgroundColor: alpha(theme.colors.gray[1], 0.7)
+            },
+            '&:active': {
+              transform: 'scale(0.85)'
+            }
+          })}
+        >
+          <Group gap={4}>
+            <Avatar src={user.profilePictureUrl ?? userPlaceholder} />
+            <IconCaretDownFilled size={12} />
+          </Group>
+        </UnstyledButton>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>
+          <Stack gap={0}>
+            <Text fw={400} c={'black'}>
+              {user.name}
+            </Text>
+            <Text fz={'xs'} fw={300}>
+              {user.email}
+            </Text>
+          </Stack>
+        </Menu.Label>
+
+        <Menu.Divider />
+
+        <Menu.Item leftSection={<IconUser size={14} />} onClick={openAccount}>
+          Account
+        </Menu.Item>
+        <Menu.Item leftSection={<IconSettings size={14} />} onClick={openSettings}>
+          Settings
+        </Menu.Item>
+        <Menu.Item leftSection={<IconLogout2 size={14} />} onClick={handleSignOut}>
+          Sign Out
+        </Menu.Item>
+      </Menu.Dropdown>
+
+      <AccountModal opened={openedAccount} onClose={closeAccount} user={user} />
+      <SettingsModal opened={openedSettings} onClose={closeSettings} user={user} />
+    </Menu>
+  )
+}
+
+export default TopbarUser
