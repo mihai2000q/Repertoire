21f20072d4fc36bc72cfe81fb60a80927526c54b
Cris <filipcristimihail@yahoo.com>
2025-02-19 15:00:23 +0100

    #373 UI - New Form Select Component - Artist Select

---
+++ repertoire.ui/src/components/@ui/form/select/ArtistSelect.test.tsx
@@ -0,0 +1,75 @@
+import { emptyArtist, reduxRender } from '../../../../test-utils.tsx'
+import { screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { expect } from 'vitest'
+import Artist from '../../../../types/models/Artist.ts'
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+import WithTotalCountResponse from '../../../../types/responses/WithTotalCountResponse.ts'
+import ArtistSelect from './ArtistSelect.tsx'
+
+describe('Band Member Select', () => {
+  const artists: Artist[] = [
+    {
+      ...emptyArtist,
+      id: '1',
+      name: 'Chester'
+    },
+    {
+      ...emptyArtist,
+      id: '2',
+      name: 'Michael'
+    },
+    {
+      ...emptyArtist,
+      id: '3',
+      name: 'Luther'
+    }
+  ]
+
+  const handlers = [
+    http.get('/artists', async () => {
+      const response: WithTotalCountResponse<Artist> = {
+        models: artists,
+        totalCount: artists.length
+      }
+      return HttpResponse.json(response)
+    })
+  ]
+
+  const server = setupServer(...handlers)
+
+  beforeAll(() => server.listen())
+
+  afterEach(() => server.resetHandlers())
+
+  afterAll(() => server.close())
+
+  it('should render and change artists', async () => {
+    const user = userEvent.setup()
+
+    const newArtist = artists[0]
+
+    const setArtist = vitest.fn()
+
+    const [{ rerender }] = reduxRender(<ArtistSelect artist={null} setArtist={setArtist} />)
+
+    const select = screen.getByRole('textbox', { name: /artist/i })
+    expect(select).toHaveValue('')
+    await user.click(select)
+
+    for (const artist of artists) {
+      expect(await screen.findByRole('option', { name: artist.name })).toBeInTheDocument()
+    }
+
+    const selectedOption = screen.getByRole('option', { name: newArtist.name })
+    await user.click(selectedOption)
+
+    expect(setArtist).toHaveBeenCalledOnce()
+    expect(setArtist).toHaveBeenCalledWith(newArtist)
+
+    rerender(<ArtistSelect artist={newArtist} setArtist={setArtist} />)
+
+    expect(screen.getByRole('textbox', { name: /artist/i })).toHaveValue(newArtist.name)
+  })
+})
---
+++ repertoire.ui/src/components/@ui/form/select/ArtistSelect.tsx
@@ -0,0 +1,136 @@
+import {
+  Avatar,
+  Combobox,
+  Group,
+  HoverCard,
+  LoadingOverlay,
+  ScrollArea,
+  Text,
+  TextInput,
+  TextInputProps,
+  useCombobox
+} from '@mantine/core'
+import Artist from '../../../../types/models/Artist.ts'
+import { IconUserFilled } from '@tabler/icons-react'
+import { useState } from 'react'
+import { useGetArtistsQuery } from '../../../../state/api/artistsApi.ts'
+import artistPlaceholder from '../../../../assets/user-placeholder.jpg'
+import {useDebouncedValue} from "@mantine/hooks";
+
+interface ArtistSelectProps extends TextInputProps {
+  artist: Artist | null
+  setArtist: (artist: Artist | null) => void
+}
+
+function ArtistSelect({ artist, setArtist, ...others }: ArtistSelectProps) {
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption()
+  })
+
+  const [value, setValue] = useState(artist?.name ?? '')
+  const [search, setSearch] = useState(artist?.name ?? '')
+  const [searchQuery] = useDebouncedValue(search, 200)
+
+  const { data: artists, isFetching } = useGetArtistsQuery({
+    currentPage: 1,
+    pageSize: 10,
+    orderBy: ['name asc'],
+    searchBy: searchQuery.trim() !== '' ? [`name ~* '${searchQuery.trim()}'`] : []
+  })
+
+  const ArtistHoverCard = () => (
+    <HoverCard withArrow={true} openDelay={200} position="bottom" shadow={'md'}>
+      <HoverCard.Target>
+        <Avatar size={23} src={artist.imageUrl ?? artistPlaceholder} alt={artist.name} />
+      </HoverCard.Target>
+      <HoverCard.Dropdown>
+        <Group gap={'xs'} maw={200} wrap={'nowrap'}>
+          <Avatar size={'md'} src={artist.imageUrl ?? artistPlaceholder} alt={artist.name} />
+          <Text inline fw={500} lineClamp={2}>
+            {artist.name}
+          </Text>
+        </Group>
+      </HoverCard.Dropdown>
+    </HoverCard>
+  )
+
+  const ArtistOption = ({ localArtist }: { localArtist: Artist }) => (
+    <Combobox.Option
+      key={localArtist.id}
+      value={localArtist.name}
+      aria-label={localArtist.name}
+      onClick={() => setArtist(artist === localArtist ? null : localArtist)}
+    >
+      <Group gap={'xs'} wrap={'nowrap'}>
+        <Avatar
+          size={'sm'}
+          src={localArtist.imageUrl ?? artistPlaceholder}
+          alt={localArtist.name}
+        />
+        <Text inline fw={500} lineClamp={2}>
+          {localArtist.name}
+        </Text>
+      </Group>
+    </Combobox.Option>
+  )
+
+  function handleSubmit(valueString: string) {
+    setValue(valueString)
+    setSearch(valueString)
+    combobox.closeDropdown()
+  }
+
+  function handleClear() {
+    setValue('')
+    setSearch('')
+    setArtist(null)
+  }
+
+  return (
+    <Combobox onOptionSubmit={handleSubmit} store={combobox}>
+      <Combobox.Target>
+        <TextInput
+          flex={1}
+          maxLength={100}
+          label={'Artist'}
+          placeholder={'Choose an artist'}
+          leftSection={artist ? <ArtistHoverCard /> : <IconUserFilled size={20} />}
+          rightSection={
+            artist ? <Combobox.ClearButton onClear={handleClear} /> : <Combobox.Chevron />
+          }
+          value={search}
+          onChange={(e) => {
+            combobox.openDropdown()
+            combobox.updateSelectedOptionIndex()
+            setSearch(e.currentTarget.value)
+          }}
+          onClick={() => combobox.openDropdown()}
+          onFocus={() => combobox.openDropdown()}
+          onBlur={() => {
+            combobox.closeDropdown()
+            setSearch(value)
+          }}
+          {...others}
+        />
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <LoadingOverlay visible={isFetching} />
+
+        <Combobox.Options>
+          <ScrollArea.Autosize mah={200} scrollbarSize={5}>
+            {artists?.totalCount === 0 && search.trim() === '' ? (
+              <Combobox.Empty>There are no artists</Combobox.Empty>
+            ) : artists?.totalCount === 0 ? (
+              <Combobox.Empty>No artists found</Combobox.Empty>
+            ) : (
+              artists?.models.map((artist) => <ArtistOption key={artist.id} localArtist={artist} />)
+            )}
+          </ScrollArea.Autosize>
+        </Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  )
+}
+
+export default ArtistSelect
