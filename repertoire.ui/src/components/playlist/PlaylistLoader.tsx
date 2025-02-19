00bbe2f80a051a2097ff3b72789420c4ffa76e78
Cris <filipcristimihail@yahoo.com>
2025-02-19 21:56:27 +0100

    #373 UI - Playlist - Add Responsivness

--- repertoire.ui/src/components/playlist/PlaylistLoader.tsx
+++ repertoire.ui/src/components/playlist/PlaylistLoader.tsx
@@ -1,34 +1,36 @@
-import { Divider, Group, Skeleton, Stack } from '@mantine/core'
+import {Card, Divider, Group, Skeleton, Stack} from '@mantine/core'
 
 function PlaylistLoader() {
   return (
     <Stack px={'xl'} data-testid={'playlist-loader'}>
-      <Group align={'start'}>
-        <Skeleton radius={'lg'} w={150} h={150} />
+      <Group>
+        <Skeleton radius={'lg'} w={'max(12vw, 150px)'} h={'max(12vw, 150px)'} />
         <Stack gap={'xxs'} pt={'md'}>
           <Skeleton w={80} h={15} />
-          <Skeleton w={200} h={45} my={4} />
+          <Skeleton w={'max(25vw, 200px)'} h={'max(4vw, 48px)'} my={'xs'} />
           <Skeleton w={85} h={15} />
         </Stack>
       </Group>
 
       <Divider />
 
-      <Stack p={'sm'} mx={'xs'} mb={'lg'} mt={'xs'}>
-        <Group gap={'xs'}>
-          <Skeleton w={50} h={15} />
-          <Skeleton w={100} h={12} />
-        </Group>
+      <Card variant={'panel'} p={0} mx={'xs'} mb={'lg'}>
         <Stack gap={0}>
-          {Array.from(Array(5)).map((_, i) => (
-            <Group key={i} px={'md'} py={'xs'}>
-              <Skeleton radius={'sm'} w={15} h={16} />
-              <Skeleton radius={'8px'} w={38} h={38} />
-              <Skeleton maw={170} miw={100} h={15} />
-            </Group>
-          ))}
+          <Group px={'md'} pt={'md'} pb={'xs'} gap={'xs'}>
+            <Skeleton w={50} h={15} />
+            <Skeleton w={100} h={12} />
+          </Group>
+          <Stack gap={0} px={'md'} py={'xs'}>
+            {Array.from(Array(5)).map((_, i) => (
+              <Group key={i} px={'md'} py={'xs'}>
+                <Skeleton radius={'sm'} w={15} h={16} mr={'xxs'} />
+                <Skeleton radius={'8px'} w={38} h={38} />
+                <Skeleton maw={170} miw={100} h={15} />
+              </Group>
+            ))}
+          </Stack>
         </Stack>
-      </Stack>
+      </Card>
     </Stack>
   )
 }
