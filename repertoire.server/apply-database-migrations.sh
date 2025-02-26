d2ff6cf8c45b998b83f29e8291a87fc70a9cf62a
Cris <filipcristimihail@yahoo.com>
2025-02-26 22:00:13 +0100

    #383 Server - Add Search Engine Migrations

--- repertoire.server/apply-database-migrations.sh
+++ repertoire.server/apply-database-migrations.sh
@@ -2,9 +2,12 @@ DIRECTORY_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source ${DIRECTORY_SCRIPT}/.env
 
+echo -e "\nDatabase Migrations running..."
+
 export GOOSE_DRIVER=postgres
 export GOOSE_DBSTRING=postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export GOOSE_MIGRATION_DIR=${DIRECTORY_SCRIPT}/migrations/database/
 
 goose up
 
+echo -e "\nDatabase Migrations done!"
