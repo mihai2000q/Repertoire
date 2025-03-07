7cbc7d60bce4e8ca4cef6b24dfbb968f07c62c95
Cris <filipcristimihail@yahoo.com>
2025-03-07 13:01:08 +0100

    #387 Server - Data - Refactor Publisher to Message Publisher Service

--- repertoire.server/data/module.go
+++ repertoire.server/data/module.go
@@ -23,6 +23,7 @@ var repositories = fx.Options(
 var services = fx.Options(
 	fx.Provide(service.NewBCryptService),
 	fx.Provide(service.NewJwtService),
+	fx.Provide(service.NewMessagePublisherService),
 	fx.Provide(service.NewSearchEngineService),
 	fx.Provide(service.NewStorageService),
 )
