1e24875296e77fd2c6a88323b618fae69a312af7
Cris <filipcristimihail@yahoo.com>
2025-03-13 18:01:14 +0100

    #387 Server - Integration Testing - Add 3 tests for the artist created, deleted and updated message handlers

---
+++ repertoire.server/test/integration/test/data/artist/artist_search_data.go
@@ -0,0 +1,91 @@
+package artist
+
+import (
+	"github.com/google/uuid"
+	"repertoire/server/internal"
+	"repertoire/server/internal/enums"
+	"repertoire/server/model"
+	"time"
+)
+
+func GetSearchDocuments() []any {
+	var documents []any
+	documents = append(documents, SongSearches...)
+	documents = append(documents, AlbumSearches...)
+	return documents
+}
+
+var UserSearchID = uuid.New()
+var ArtistSearchID = uuid.New()
+
+var AlbumSearches = []any{
+	model.AlbumSearch{
+		Title:     "Album 1",
+		ImageUrl:  &[]internal.FilePath{"song-image.png"}[0],
+		UpdatedAt: time.Now().UTC(),
+		Artist: &model.AlbumArtistSearch{
+			ID:        ArtistSearchID,
+			Name:      "Artist 1",
+			ImageUrl:  &[]internal.FilePath{"song/artist-image.png"}[0],
+			UpdatedAt: time.Now().UTC(),
+		},
+		SearchBase: model.SearchBase{
+			ID:     "album-" + uuid.New().String(),
+			Type:   enums.Album,
+			UserID: UserSearchID,
+		},
+	},
+	model.AlbumSearch{
+		Title: "Album 2",
+		Artist: &model.AlbumArtistSearch{
+			ID:        ArtistSearchID,
+			Name:      "Artist 1",
+			ImageUrl:  &[]internal.FilePath{"song/artist-image.png"}[0],
+			UpdatedAt: time.Now().UTC(),
+		},
+		SearchBase: model.SearchBase{
+			ID:     "song-" + uuid.New().String(),
+			Type:   enums.Song,
+			UserID: UserSearchID,
+		},
+	},
+}
+
+var SongSearches = []any{
+	model.SongSearch{
+		Title:     "Song 1",
+		ImageUrl:  &[]internal.FilePath{"song-image.png"}[0],
+		UpdatedAt: time.Now().UTC(),
+		Artist: &model.SongArtistSearch{
+			ID:        ArtistSearchID,
+			Name:      "Artist 1",
+			ImageUrl:  &[]internal.FilePath{"song/artist-image.png"}[0],
+			UpdatedAt: time.Now().UTC(),
+		},
+		Album: &model.SongAlbumSearch{
+			ID:        uuid.New(),
+			Title:     "Album 1",
+			ImageUrl:  &[]internal.FilePath{"song/artist-image.png"}[0],
+			UpdatedAt: time.Now().UTC(),
+		},
+		SearchBase: model.SearchBase{
+			ID:     "song-" + uuid.New().String(),
+			Type:   enums.Song,
+			UserID: UserSearchID,
+		},
+	},
+	model.SongSearch{
+		Title: "Song 2",
+		Artist: &model.SongArtistSearch{
+			ID:        ArtistSearchID,
+			Name:      "Artist 1",
+			ImageUrl:  &[]internal.FilePath{"song/artist-image.png"}[0],
+			UpdatedAt: time.Now().UTC(),
+		},
+		SearchBase: model.SearchBase{
+			ID:     "song-" + uuid.New().String(),
+			Type:   enums.Song,
+			UserID: UserSearchID,
+		},
+	},
+}
