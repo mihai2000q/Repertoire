a82e73fd3dfb75be4c50ad703ec0b701f176aa1c
Cris <filipcristimihail@yahoo.com>
2025-03-26 23:16:17 +0100

    #423 Server - Data: Meili Task Tracker Service - a tracking method using cache to track tasks from meilisearch by user

---
+++ repertoire.server/data/service/meili_task_tracker_service.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"repertoire/server/data/cache"
+	"time"
+)
+
+type MeiliTaskTrackerService interface {
+	Track(taskID string, userID string)
+	GetUserID(taskID string) (string, bool)
+}
+
+type meiliTaskTrackerService struct {
+	cache cache.Cache
+}
+
+func NewMeiliTaskTrackerService(cache cache.Cache) MeiliTaskTrackerService {
+	return meiliTaskTrackerService{cache: cache}
+}
+
+func (d meiliTaskTrackerService) Track(taskID string, userID string) {
+	d.cache.Set(taskID, userID, time.Minute)
+}
+
+func (d meiliTaskTrackerService) GetUserID(taskID string) (string, bool) {
+	userID, found := d.cache.Get(taskID)
+	if found {
+		return userID.(string), found
+	}
+	return "", found
+}
