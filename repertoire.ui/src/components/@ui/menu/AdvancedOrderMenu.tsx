852bea35077754c461d9382b2b7cea08438f2c5a
Cris <filipcristimihail@yahoo.com>
2025-04-15 20:11:04 +0200

    #453 UI - Menu Components - Advanced Order Menu - Do not reorder if the destination and source are equal and Add Scrollbar

--- repertoire.ui/src/components/@ui/menu/AdvancedOrderMenu.tsx
+++ repertoire.ui/src/components/@ui/menu/AdvancedOrderMenu.tsx
@@ -1,6 +1,17 @@
 import Order from '../../../types/Order.ts'
 import { IconArrowNarrowDown, IconArrowNarrowUp } from '@tabler/icons-react'
-import { ActionIcon, alpha, Center, Group, Menu, Space, Stack, Text, Tooltip } from '@mantine/core'
+import {
+  ActionIcon,
+  alpha,
+  Center,
+  Group,
+  Menu,
+  ScrollArea,
+  Space,
+  Stack,
+  Text,
+  Tooltip
+} from '@mantine/core'
 import { MouseEvent, ReactElement, ReactNode, useState } from 'react'
 import OrderType from '../../../utils/enums/OrderType.ts'
 import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
@@ -38,6 +49,8 @@ function AdvancedOrderMenu({ children, orders, setOrders, propertyIcons }: Advan
     const from = source.index
     const to = destination?.index || 0
 
+    if (from === to) return
+
     const newOrders = [...orders]
     const item = newOrders[from]
     newOrders.splice(from, 1)
@@ -50,108 +63,115 @@ function AdvancedOrderMenu({ children, orders, setOrders, propertyIcons }: Advan
       <Menu.Target>{children}</Menu.Target>
 
       <Menu.Dropdown>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="dnd-list" direction="vertical">
-            {(provided) => (
-              <Stack gap={'xxs'} ref={provided.innerRef} {...provided.droppableProps}>
-                {orders.map((order, index) => (
-                  <Draggable key={order.label} index={index} draggableId={order.label}>
-                    {(provided, snapshot) => {
-                      if (snapshot.isDragging) {
-                        if ('left' in provided.draggableProps.style) {
-                          provided.draggableProps.style.left = 0
+        <ScrollArea scrollbars={'y'} scrollbarSize={7}>
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="dnd-list" direction="vertical">
+              {(provided) => (
+                <Stack
+                  gap={'xxs'}
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  style={{ maxHeight: '40vh' }}
+                >
+                  {orders.map((order, index) => (
+                    <Draggable key={order.label} index={index} draggableId={order.label}>
+                      {(provided, snapshot) => {
+                        if (snapshot.isDragging) {
+                          if ('left' in provided.draggableProps.style) {
+                            provided.draggableProps.style.left = 0
+                          }
+                          if ('top' in provided.draggableProps.style) {
+                            provided.draggableProps.style.top -= 160
+                          }
                         }
-                        if ('top' in provided.draggableProps.style) {
-                          provided.draggableProps.style.top -= 160
-                        }
-                      }
 
-                      return (
-                        <Group
-                          role={'button'}
-                          aria-label={order.label}
-                          ref={provided.innerRef}
-                          data-active={order.checked ?? false}
-                          gap={0}
-                          sx={(theme) => ({
-                            transition: '0.2s',
-                            borderRadius: theme.radius.md,
-                            color: theme.colors.gray[7],
-                            '&:hover': {
+                        return (
+                          <Group
+                            role={'button'}
+                            aria-label={order.label}
+                            ref={provided.innerRef}
+                            data-active={order.checked ?? false}
+                            gap={0}
+                            sx={(theme) => ({
+                              transition: '0.2s',
+                              borderRadius: theme.radius.md,
                               color: theme.colors.gray[7],
-                              backgroundColor: theme.colors.gray[1]
-                            },
-
-                            ...(order.checked && {
-                              color: theme.colors.primary[4],
-                              backgroundColor: alpha(theme.colors.primary[1], 0.4),
                               '&:hover': {
-                                color: isOnlyOneOrderActive
-                                  ? theme.colors.primary[4]
-                                  : theme.colors.primary[5],
-                                backgroundColor: isOnlyOneOrderActive
-                                  ? alpha(theme.colors.primary[1], 0.4)
-                                  : alpha(theme.colors.primary[1], 0.8)
-                              }
-                            })
-                          })}
-                          px={12}
-                          py={8}
-                          {...provided.dragHandleProps}
-                          {...provided.draggableProps}
-                          style={{
-                            ...provided.draggableProps.style,
-                            cursor: snapshot.isDragging
-                              ? 'grabbing'
-                              : order.checked === true && isOnlyOneOrderActive
-                                ? 'default'
-                                : 'pointer'
-                          }}
-                          onClick={() => onClick(order, index)}
-                        >
-                          <Center w={15} h={15}>
-                            {propertyIcons?.get(order.property)}
-                          </Center>
-                          <Text
-                            fz={'sm'}
-                            c={order.checked === true ? 'primary.4' : 'dark'}
-                            inline
-                            pl={8}
-                            pr={4}
-                          >
-                            {order.label}
-                          </Text>
-                          <Space flex={1} />
-                          {order.checked === true && (
-                            <Tooltip label={'Change Order'} openDelay={200}>
-                              <ActionIcon
-                                variant={'subtle'}
-                                size={'xs'}
-                                aria-label={
-                                  order.type !== OrderType.Descending
-                                    ? 'change-order-ascending'
-                                    : 'change-order-descending'
+                                color: theme.colors.gray[7],
+                                backgroundColor: theme.colors.gray[1]
+                              },
+
+                              ...(order.checked && {
+                                color: theme.colors.primary[4],
+                                backgroundColor: alpha(theme.colors.primary[1], 0.4),
+                                '&:hover': {
+                                  color: isOnlyOneOrderActive
+                                    ? theme.colors.primary[4]
+                                    : theme.colors.primary[5],
+                                  backgroundColor: isOnlyOneOrderActive
+                                    ? alpha(theme.colors.primary[1], 0.4)
+                                    : alpha(theme.colors.primary[1], 0.8)
                                 }
-                                onClick={(e) => onTypeUpdate(e, order, index)}
-                              >
-                                {order.type !== OrderType.Descending ? (
-                                  <IconArrowNarrowUp size={16} />
-                                ) : (
-                                  <IconArrowNarrowDown size={16} />
-                                )}
-                              </ActionIcon>
-                            </Tooltip>
-                          )}
-                        </Group>
-                      )
-                    }}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </Stack>
-            )}
-          </Droppable>
-        </DragDropContext>
+                              })
+                            })}
+                            px={12}
+                            py={8}
+                            {...provided.dragHandleProps}
+                            {...provided.draggableProps}
+                            style={{
+                              ...provided.draggableProps.style,
+                              cursor: snapshot.isDragging
+                                ? 'grabbing'
+                                : order.checked === true && isOnlyOneOrderActive
+                                  ? 'default'
+                                  : 'pointer'
+                            }}
+                            onClick={() => onClick(order, index)}
+                          >
+                            <Center w={15} h={15}>
+                              {propertyIcons?.get(order.property)}
+                            </Center>
+                            <Text
+                              fz={'sm'}
+                              c={order.checked === true ? 'primary.4' : 'dark'}
+                              inline
+                              pl={8}
+                              pr={4}
+                            >
+                              {order.label}
+                            </Text>
+                            <Space flex={1} />
+                            {order.checked === true && (
+                              <Tooltip label={'Change Order'} openDelay={200}>
+                                <ActionIcon
+                                  variant={'subtle'}
+                                  size={'xs'}
+                                  aria-label={
+                                    order.type !== OrderType.Descending
+                                      ? 'change-order-ascending'
+                                      : 'change-order-descending'
+                                  }
+                                  onClick={(e) => onTypeUpdate(e, order, index)}
+                                >
+                                  {order.type !== OrderType.Descending ? (
+                                    <IconArrowNarrowUp size={16} />
+                                  ) : (
+                                    <IconArrowNarrowDown size={16} />
+                                  )}
+                                </ActionIcon>
+                              </Tooltip>
+                            )}
+                          </Group>
+                        )
+                      }}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </Stack>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </ScrollArea>
       </Menu.Dropdown>
     </Menu>
   )
