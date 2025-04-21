587987cbc839ae33268805dd4404b09a76eacfdb
Cris <filipcristimihail@yahoo.com>
2025-04-21 21:36:12 +0200

    #461 UI - Artists/Albums/Songs/Playlists - Add Filters to Data

---
+++ repertoire.ui/src/data/artists/artistsFilters.ts
@@ -0,0 +1,75 @@
+import ArtistProperty from '../../types/enums/ArtistProperty.ts'
+import Filter, { FilterValue } from '../../types/Filter.ts'
+import FilterOperator from '../../types/enums/FilterOperator.ts'
+import { ArtistFiltersMetadata } from '../../types/models/FiltersMetadata.ts'
+
+const artistsFilters: Filter[] = [
+  {
+    property: ArtistProperty.BandMembers,
+    operator: FilterOperator.GreaterThanOrEqual,
+    isSet: false
+  },
+  { property: ArtistProperty.BandMembers, operator: FilterOperator.LessThanOrEqual, isSet: false },
+
+  { property: ArtistProperty.Band, operator: FilterOperator.Equal, isSet: false, value: true },
+  { property: ArtistProperty.Band, operator: FilterOperator.NotEqual, isSet: false, value: true },
+
+  { property: ArtistProperty.Albums, operator: FilterOperator.GreaterThanOrEqual, isSet: false },
+  { property: ArtistProperty.Albums, operator: FilterOperator.LessThanOrEqual, isSet: false },
+
+  { property: ArtistProperty.Songs, operator: FilterOperator.GreaterThanOrEqual, isSet: false },
+  { property: ArtistProperty.Songs, operator: FilterOperator.LessThanOrEqual, isSet: false },
+
+  {
+    property: ArtistProperty.Rehearsals,
+    operator: FilterOperator.GreaterThanOrEqual,
+    isSet: false
+  },
+  { property: ArtistProperty.Rehearsals, operator: FilterOperator.LessThanOrEqual, isSet: false },
+
+  {
+    property: ArtistProperty.Confidence,
+    operator: FilterOperator.GreaterThanOrEqual,
+    isSet: false
+  },
+  { property: ArtistProperty.Confidence, operator: FilterOperator.LessThanOrEqual, isSet: false },
+
+  { property: ArtistProperty.Progress, operator: FilterOperator.GreaterThanOrEqual, isSet: false },
+  { property: ArtistProperty.Progress, operator: FilterOperator.LessThanOrEqual, isSet: false },
+
+  {
+    property: ArtistProperty.LastPlayed,
+    operator: FilterOperator.GreaterThanOrEqual,
+    isSet: false
+  },
+  { property: ArtistProperty.LastPlayed, operator: FilterOperator.LessThanOrEqual, isSet: false },
+  { property: ArtistProperty.LastPlayed, operator: FilterOperator.IsNull, isSet: false },
+  { property: ArtistProperty.LastPlayed, operator: FilterOperator.IsNotNull, isSet: false }
+]
+
+export const artistsFiltersMetadataMap: (
+  metadata: ArtistFiltersMetadata
+) => [string, FilterValue][] = (metadata) => [
+  [ArtistProperty.BandMembers + FilterOperator.GreaterThanOrEqual, metadata.minBandMembersCount],
+  [ArtistProperty.BandMembers + FilterOperator.LessThanOrEqual, metadata.maxBandMembersCount],
+
+  [ArtistProperty.Albums + FilterOperator.GreaterThanOrEqual, metadata.minAlbumsCount],
+  [ArtistProperty.Albums + FilterOperator.LessThanOrEqual, metadata.maxAlbumsCount],
+
+  [ArtistProperty.Songs + FilterOperator.GreaterThanOrEqual, metadata.minSongsCount],
+  [ArtistProperty.Songs + FilterOperator.LessThanOrEqual, metadata.maxSongsCount],
+
+  [ArtistProperty.Rehearsals + FilterOperator.GreaterThanOrEqual, metadata.minRehearsals],
+  [ArtistProperty.Rehearsals + FilterOperator.LessThanOrEqual, metadata.maxRehearsals],
+
+  [ArtistProperty.Confidence + FilterOperator.GreaterThanOrEqual, metadata.minConfidence],
+  [ArtistProperty.Confidence + FilterOperator.LessThanOrEqual, metadata.maxConfidence],
+
+  [ArtistProperty.Progress + FilterOperator.GreaterThanOrEqual, metadata.minProgress],
+  [ArtistProperty.Progress + FilterOperator.LessThanOrEqual, metadata.maxProgress],
+
+  [ArtistProperty.LastPlayed + FilterOperator.GreaterThanOrEqual, metadata.minLastTimePlayed],
+  [ArtistProperty.LastPlayed + FilterOperator.LessThanOrEqual, metadata.maxLastTimePlayed]
+]
+
+export default artistsFilters
