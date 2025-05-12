cb6af20f7efd10430c19b49efc6c7190519a9521
Cris <filipcristimihail@yahoo.com>
2025-05-12 23:00:31 +0200

    #496 UI - Artist - Add New Song Modal - Add New Optional Fields: Album, Release Date, Guitar Tuning, Difficulty, Bpm, Songsterr and Youtube

--- repertoire.ui/src/components/artist/modal/AddNewArtistSongModal.test.tsx
+++ repertoire.ui/src/components/artist/modal/AddNewArtistSongModal.test.tsx
@@ -5,15 +5,68 @@ import { act, screen, waitFor } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import { http, HttpResponse } from 'msw'
 import { CreateSongRequest } from '../../../types/requests/SongRequests.ts'
+import { GuitarTuning } from '../../../types/models/Song.ts'
+import Difficulty from '../../../types/enums/Difficulty.ts'
+import { AlbumSearch } from '../../../types/models/Search.ts'
+import SearchType from '../../../types/enums/SearchType.ts'
+import WithTotalCountResponse from '../../../types/responses/WithTotalCountResponse.ts'
+import dayjs from 'dayjs'
 
 describe('Add New Artist Song Modal', () => {
-  const server = setupServer()
+  const guitarTunings: GuitarTuning[] = [
+    {
+      id: '1',
+      name: 'E Standard'
+    },
+    {
+      id: '2',
+      name: 'Drop D'
+    },
+    {
+      id: '3',
+      name: 'Drop A'
+    }
+  ]
+
+  const albums: AlbumSearch[] = [
+    {
+      id: '1',
+      title: 'Album 1',
+      type: SearchType.Album
+    },
+    {
+      id: '2',
+      title: 'Album 2',
+      type: SearchType.Album,
+      imageUrl: 'image.png',
+      releaseDate: new Date().toISOString()
+    }
+  ]
+
+  const handlers = [
+    http.get('/search', async () => {
+      const response: WithTotalCountResponse<AlbumSearch> = {
+        models: albums,
+        totalCount: albums.length
+      }
+      return HttpResponse.json(response)
+    }),
+    http.get('/songs/guitar-tunings', async () => {
+      return HttpResponse.json(guitarTunings)
+    })
+  ]
+
+  const server = setupServer(...handlers)
 
   beforeAll(() => server.listen())
 
   afterEach(() => server.resetHandlers())
 
   afterAll(() => server.close())
+
+  const validSongsterrLink =
+    'https://www.songsterr.com/a/wsa/metallica-master-of-puppets-tab-s455118'
+  const validYoutubeLink = 'https://www.youtube.com/watch?v=E0ozmU9cJDg'
 
   it('should render', () => {
     reduxRender(<AddNewArtistSongModal opened={true} onClose={() => {}} artistId={''} />)
@@ -59,40 +112,172 @@ describe('Add New Artist Song Modal', () => {
     expect(screen.getByRole('textbox', { name: /title/i })).toHaveValue('')
   })
 
-  it('should send only create request when no image is uploaded', async () => {
-    const user = userEvent.setup()
+  describe('should send only create request when no image is uploaded', () => {
+    it('minimal', async () => {
+      const user = userEvent.setup()
 
-    const artistId = 'some-artist-id'
-    const newTitle = 'New Song'
+      const artistId = 'some-artist-id'
+      const newTitle = 'New Song'
 
-    const onClose = vitest.fn()
+      const onClose = vitest.fn()
 
-    let capturedRequest: CreateSongRequest
-    server.use(
-      http.post('/songs', async (req) => {
-        capturedRequest = (await req.request.json()) as CreateSongRequest
-        return HttpResponse.json({ message: 'it worked' })
-      })
-    )
+      let capturedRequest: CreateSongRequest
+      server.use(
+        http.post('/songs', async (req) => {
+          capturedRequest = (await req.request.json()) as CreateSongRequest
+          return HttpResponse.json({ message: 'it worked' })
+        })
+      )
 
-    reduxRender(
-      withToastify(<AddNewArtistSongModal opened={true} onClose={onClose} artistId={artistId} />)
-    )
+      reduxRender(
+        withToastify(<AddNewArtistSongModal opened={true} onClose={onClose} artistId={artistId} />)
+      )
 
-    await user.type(screen.getByRole('textbox', { name: /title/i }), newTitle)
-    await user.click(screen.getByRole('button', { name: /submit/i }))
+      await user.type(screen.getByRole('textbox', { name: /title/i }), newTitle)
+      await user.click(screen.getByRole('button', { name: /submit/i }))
 
-    await waitFor(() =>
-      expect(capturedRequest).toStrictEqual({
-        title: newTitle,
-        description: '',
-        artistId: artistId
-      })
-    )
-    expect(onClose).toHaveBeenCalledOnce()
+      await waitFor(() =>
+        expect(capturedRequest).toStrictEqual({
+          title: newTitle,
+          description: '',
+          artistId: artistId
+        })
+      )
+      expect(onClose).toHaveBeenCalledOnce()
 
-    expect(screen.getByText(`${newTitle} added!`))
-    expect(screen.getByRole('textbox', { name: /title/i })).toHaveValue('')
+      expect(screen.getByText(`${newTitle} added!`))
+      expect(screen.getByRole('textbox', { name: /title/i })).toHaveValue('')
+    })
+
+    it('with optional fields', async () => {
+      const user = userEvent.setup()
+
+      const now = new Date()
+
+      const newTitle = 'New Song'
+      const newAlbum = albums[0]
+      const newReleaseDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0)
+      const newGuitarTuning = guitarTunings[1]
+      const newDifficulty = Difficulty.Medium
+      const newBpm = 123
+
+      const onClose = vitest.fn()
+      const artistId = 'some-id'
+
+      let capturedRequest: CreateSongRequest
+      server.use(
+        http.post('/songs', async (req) => {
+          capturedRequest = (await req.request.json()) as CreateSongRequest
+          return HttpResponse.json({ message: 'it worked' })
+        })
+      )
+
+      reduxRender(
+        withToastify(<AddNewArtistSongModal opened={true} onClose={onClose} artistId={artistId} />)
+      )
+
+      await user.type(screen.getByRole('textbox', { name: /title/i }), newTitle)
+
+      // fill form
+      await user.click(screen.getByRole('button', { name: 'album' }))
+      await user.click(screen.getByRole('option', { name: newAlbum.title }))
+      expect(screen.getByRole('button', { name: newAlbum.title })).toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: 'release-date' }))
+      await user.click(
+        screen.getByRole('button', { name: dayjs(newReleaseDate).format('D MMMM YYYY') })
+      )
+      expect(screen.getByRole('button', { name: 'release-date' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: 'guitar-tuning' }))
+      await user.click(screen.getByRole('option', { name: newGuitarTuning.name }))
+      expect(screen.getByRole('button', { name: 'guitar-tuning' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: 'difficulty' }))
+      await user.click(screen.getByRole('option', { name: newDifficulty }))
+      expect(screen.getByRole('button', { name: 'difficulty' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: 'bpm' }))
+      await user.type(screen.getByRole('textbox', { name: 'bpm' }), newBpm.toString())
+      expect(screen.getByRole('button', { name: 'bpm' })).toHaveAttribute('aria-selected', 'true')
+
+      await user.click(screen.getByRole('button', { name: 'songsterr' }))
+      await user.click(screen.getByRole('textbox', { name: 'songsterr' }))
+      await user.paste(validSongsterrLink)
+      expect(screen.getByRole('button', { name: 'songsterr' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: 'youtube' }))
+      await user.click(screen.getByRole('textbox', { name: 'youtube' }))
+      await user.paste(validYoutubeLink)
+      expect(screen.getByRole('button', { name: 'youtube' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: /submit/i }))
+
+      await waitFor(() =>
+        expect(capturedRequest).toStrictEqual({
+          title: newTitle,
+          description: '',
+          albumId: newAlbum.id,
+          releaseDate: newReleaseDate.toISOString(),
+          guitarTuningId: newGuitarTuning.id,
+          difficulty: newDifficulty,
+          bpm: newBpm,
+          songsterrLink: validSongsterrLink,
+          youtubeLink: validYoutubeLink
+        })
+      )
+      expect(onClose).toHaveBeenCalledOnce()
+
+      expect(screen.getByText(`${newTitle} added!`))
+
+      // reset form
+      expect(screen.getByRole('textbox', { name: /title/i })).toHaveValue('')
+
+      expect(screen.getByRole('button', { name: 'album' })).not.toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+      expect(screen.queryByRole('button', { name: newAlbum.title })).not.toBeInTheDocument()
+
+      await user.click(screen.getByRole('button', { name: 'release-date' }))
+      expect(screen.getByRole('button', { name: 'release-date' })).not.toHaveAttribute(
+        'aria-selected',
+        'true'
+      )
+
+      await user.click(screen.getByRole('button', { name: 'guitar-tuning' }))
+      expect(screen.getByRole('textbox', { name: 'search' })).toHaveValue('')
+
+      await user.click(screen.getByRole('button', { name: 'difficulty' }))
+      expect(screen.getAllByRole('textbox', { name: 'search' })[1]).toHaveValue('')
+
+      await user.click(screen.getByRole('button', { name: 'bpm' }))
+      expect(screen.getByRole('textbox', { name: 'bpm' })).toHaveValue('')
+
+      await user.click(screen.getByRole('button', { name: 'songsterr' }))
+      expect(screen.getByRole('textbox', { name: 'songsterr' })).toHaveValue('')
+
+      await user.click(screen.getByRole('button', { name: 'youtube' }))
+      expect(screen.getByRole('textbox', { name: 'youtube' })).toHaveValue('')
+    })
   })
 
   it('should send create request and save image request when the image is uploaded', async () => {
@@ -162,6 +347,19 @@ describe('Add New Artist Song Modal', () => {
     expect(onClose).toHaveBeenCalledOnce()
   })
 
+  it('should', async () => {
+    const user = userEvent.setup()
+
+    const newAlbum = albums[1]
+
+    reduxRender(<AddNewArtistSongModal opened={true} onClose={() => {}} artistId={''} />)
+
+    await user.click(screen.getByRole('button', { name: 'album' }))
+    await user.click(screen.getByRole('option', { name: newAlbum.title }))
+    expect(screen.getByText(/if no image is uploaded, it will be inherited/i)).toBeInTheDocument()
+    expect(screen.getByText(/if no release date is set, it will be inherited/i)).toBeInTheDocument()
+  })
+
   it('should validate title', async () => {
     const user = userEvent.setup()
 
@@ -179,5 +377,55 @@ describe('Add New Artist Song Modal', () => {
     await user.clear(title)
     act(() => title.blur())
     expect(title).toBeInvalid()
+  })
+
+  it("should validate songsterr and youtube links' fields", async () => {
+    const user = userEvent.setup()
+
+    reduxRender(<AddNewArtistSongModal opened={true} onClose={() => {}} artistId={''} />)
+
+    // songsterr
+    const songsterrButton = screen.getByRole('button', { name: /songsterr/i })
+    await user.click(songsterrButton)
+    const songsterr = screen.getByRole('textbox', { name: /songsterr/i })
+
+    expect(songsterrButton).not.toBeInvalid()
+    expect(songsterr).not.toBeInvalid()
+    await user.type(songsterr, 'something')
+    act(() => songsterr.blur())
+    expect(songsterr).toBeInvalid()
+    expect(songsterrButton).toBeInvalid()
+
+    await user.clear(songsterr)
+    act(() => songsterr.blur())
+    expect(songsterr).not.toBeInvalid()
+    expect(songsterrButton).not.toBeInvalid()
+
+    await user.click(songsterr)
+    await user.paste(validSongsterrLink)
+    expect(songsterr).not.toBeInvalid()
+    expect(songsterrButton).not.toBeInvalid()
+
+    // youtube
+    const youtubeButton = screen.getByRole('button', { name: /youtube/i })
+    await user.click(youtubeButton)
+    const youtube = screen.getByRole('textbox', { name: /youtube/i })
+
+    expect(youtube).not.toBeInvalid()
+    expect(youtubeButton).not.toBeInvalid()
+    await user.type(youtube, 'something')
+    act(() => youtube.blur())
+    expect(youtube).toBeInvalid()
+    expect(youtubeButton).toBeInvalid()
+
+    await user.clear(youtube)
+    act(() => youtube.blur())
+    expect(youtube).not.toBeInvalid()
+    expect(youtubeButton).not.toBeInvalid()
+
+    await user.click(youtube)
+    await user.paste(validYoutubeLink)
+    expect(youtube).not.toBeInvalid()
+    expect(youtubeButton).not.toBeInvalid()
   })
 })
--- repertoire.ui/src/components/artist/modal/AddNewArtistSongModal.tsx
+++ repertoire.ui/src/components/artist/modal/AddNewArtistSongModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, Modal, Stack, TextInput } from '@mantine/core'
+import { alpha, Button, Center, Group, Modal, Stack, Text, TextInput, Tooltip } from '@mantine/core'
 import { useState } from 'react'
 import { FileWithPath } from '@mantine/dropzone'
 import { useForm, zodResolver } from '@mantine/form'
@@ -9,6 +9,23 @@ import {
 import { toast } from 'react-toastify'
 import { useCreateSongMutation, useSaveImageToSongMutation } from '../../../state/api/songsApi.ts'
 import ImageDropzoneWithPreview from '../../@ui/image/ImageDropzoneWithPreview.tsx'
+import GuitarTuningSelectButton from '../../@ui/form/select/button/GuitarTuningSelectButton.tsx'
+import DifficultySelectButton from '../../@ui/form/select/button/DifficultySelectButton.tsx'
+import {
+  IconBrandYoutubeFilled,
+  IconCalendarRepeat,
+  IconGuitarPickFilled,
+  IconInfoCircleFilled
+} from '@tabler/icons-react'
+import NumberInputButton from '../../@ui/form/input/button/NumberInputButton.tsx'
+import CustomIconMetronome from '../../@ui/icons/CustomIconMetronome.tsx'
+import TextInputButton from '../../@ui/form/input/button/TextInputButton.tsx'
+import { GuitarTuning } from '../../../types/models/Song.ts'
+import Difficulty from '../../../types/enums/Difficulty.ts'
+import DatePickerButton from '../../@ui/form/date/DatePickerButton.tsx'
+import dayjs from 'dayjs'
+import { AlbumSearch } from '../../../types/models/Search.ts'
+import AlbumSelectButton from '../../@ui/form/select/button/AlbumSelectButton.tsx'
 
 interface AddNewArtistSongModalProps {
   opened: boolean
@@ -22,6 +39,14 @@ function AddNewArtistSongModal({ opened, onClose, artistId }: AddNewArtistSongMo
   const isLoading = isCreateSongLoading || isSaveImageLoading
 
   const [image, setImage] = useState<FileWithPath>(null)
+  const [album, setAlbum] = useState<AlbumSearch>(null)
+  const [releaseDate, setReleaseDate] = useState<Date>(null)
+  const [guitarTuning, setGuitarTuning] = useState<GuitarTuning>(null)
+  const [difficulty, setDifficulty] = useState<Difficulty>(null)
+  const [bpm, setBpm] = useState<string | number>()
+
+  const [isSongsterrLinkSelected, setIsSongsterrLinkSelected] = useState(false)
+  const [isYoutubeLinkSelected, setIsYoutubeLinkSelected] = useState(false)
 
   const onCloseWithImage = () => {
     onClose()
@@ -36,19 +61,49 @@ function AddNewArtistSongModal({ opened, onClose, artistId }: AddNewArtistSongMo
     validateInputOnBlur: true,
     validateInputOnChange: false,
     clearInputErrorOnChange: true,
-    validate: zodResolver(addNewArtistSongValidation)
+    validate: zodResolver(addNewArtistSongValidation),
+    onValuesChange: (values) => {
+      setIsSongsterrLinkSelected(
+        values.songsterrLink !== '' &&
+          values.songsterrLink !== null &&
+          values.songsterrLink !== undefined
+      )
+      setIsYoutubeLinkSelected(
+        values.youtubeLink !== '' && values.youtubeLink !== null && values.youtubeLink !== undefined
+      )
+    }
   })
 
-  async function addSong({ title }: AddNewArtistSongForm) {
+  async function addSong({ title, youtubeLink, songsterrLink }: AddNewArtistSongForm) {
     title = title.trim()
+    songsterrLink = songsterrLink?.trim() === '' ? undefined : songsterrLink?.trim()
+    youtubeLink = youtubeLink?.trim() === '' ? undefined : youtubeLink?.trim()
 
-    const res = await createSongMutation({ title, description: '', artistId }).unwrap()
+    const res = await createSongMutation({
+      title: title,
+      description: '',
+      artistId: album ? undefined : artistId,
+      albumId: album?.id,
+      releaseDate: releaseDate ?? undefined,
+      guitarTuningId: guitarTuning?.id,
+      difficulty: difficulty ?? undefined,
+      bpm: typeof bpm === 'number' ? bpm : undefined,
+      songsterrLink: songsterrLink,
+      youtubeLink: youtubeLink
+    }).unwrap()
 
     if (image) await saveImageMutation({ image: image, id: res.id }).unwrap()
 
     toast.success(`${title} added!`)
     onCloseWithImage()
     form.reset()
+    setAlbum(null)
+    setReleaseDate(null)
+    setGuitarTuning(null)
+    setDifficulty(null)
+    setBpm(null)
+    setIsSongsterrLinkSelected(false)
+    setIsYoutubeLinkSelected(false)
   }
 
   return (
@@ -56,17 +111,154 @@ function AddNewArtistSongModal({ opened, onClose, artistId }: AddNewArtistSongMo
       <Modal.Body p={'xs'}>
         <form onSubmit={form.onSubmit(addSong)}>
           <Stack>
-            <Group>
+            <Group align={'start'}>
               <ImageDropzoneWithPreview image={image} setImage={setImage} />
-              <TextInput
-                flex={1}
-                withAsterisk={true}
-                maxLength={100}
-                label="Title"
-                placeholder="The title of the song"
-                key={form.key('title')}
-                {...form.getInputProps('title')}
-              />
+
+              <Stack flex={1} gap={'xxs'}>
+                <Group gap={'xxs'}>
+                  <TextInput
+                    flex={1}
+                    withAsterisk={true}
+                    maxLength={100}
+                    label="Title"
+                    placeholder="The title of the song"
+                    key={form.key('title')}
+                    {...form.getInputProps('title')}
+                  />
+                  <AlbumSelectButton
+                    mt={form.getInputProps('title').error ? 3 : 19}
+                    size={'lg'}
+                    album={album}
+                    setAlbum={setAlbum}
+                    searchFilter={artistId ? [`artist.id = ${artistId}`] : []}
+                  />
+                </Group>
+
+                <Group gap={2}>
+                  <Tooltip.Group openDelay={500}>
+                    <DatePickerButton
+                      aria-label={'release-date'}
+                      icon={<IconCalendarRepeat size={18} />}
+                      value={releaseDate}
+                      onChange={setReleaseDate}
+                      tooltipLabels={{
+                        default: 'Select a release date',
+                        selected: (val) => `Released on ${dayjs(val).format('D MMMM YYYY')}`
+                      }}
+                    />
+                    <GuitarTuningSelectButton
+                      guitarTuning={guitarTuning}
+                      setGuitarTuning={setGuitarTuning}
+                    />
+                    <DifficultySelectButton difficulty={difficulty} setDifficulty={setDifficulty} />
+                    <NumberInputButton
+                      icon={<CustomIconMetronome size={16} />}
+                      aria-label={'bpm'}
+                      inputProps={{
+                        'aria-label': 'bpm',
+                        placeholder: 'Enter bpm',
+                        leftSection: <CustomIconMetronome size={15} />,
+                        value: bpm,
+                        onChange: setBpm
+                      }}
+                      tooltipLabels={{
+                        selected: `Bpm is ${bpm}`,
+                        default: 'Enter a bpm'
+                      }}
+                    />
+                    <TextInputButton
+                      icon={<IconGuitarPickFilled size={16} />}
+                      inputKey={form.key('songsterrLink')}
+                      aria-label={'songsterr'}
+                      inputProps={{
+                        'aria-label': 'songsterr',
+                        placeholder: 'Enter Songsterr Link',
+                        leftSection: <IconGuitarPickFilled size={15} />,
+                        w: 370,
+                        ...form.getInputProps('songsterrLink')
+                      }}
+                      isSelected={isSongsterrLinkSelected}
+                      tooltipLabels={{
+                        default: 'Enter Songsterr Link',
+                        selected: 'Songsterr Link entered!'
+                      }}
+                      variant={'transparent'}
+                      sx={(theme) => ({
+                        ...(Object.entries(theme.components.ActionIcon.styles(theme).root).find(
+                          (s) => s[0] === '&[data-variant="form"]'
+                        )[1] as object),
+
+                        '&[aria-selected="true"]': {
+                          color: theme.colors.blue[5],
+                          backgroundColor: alpha(theme.colors.blue[1], 0.5),
+
+                          '&:hover': {
+                            color: theme.colors.blue[6],
+                            backgroundColor: theme.colors.blue[1]
+                          }
+                        }
+                      })}
+                    />
+                    <TextInputButton
+                      icon={<IconBrandYoutubeFilled size={16} />}
+                      inputKey={form.key('youtubeLink')}
+                      aria-label={'youtube'}
+                      inputProps={{
+                        'aria-label': 'youtube',
+                        placeholder: 'Enter Youtube Link',
+                        leftSection: <IconBrandYoutubeFilled size={15} />,
+                        w: 300,
+                        ...form.getInputProps('youtubeLink')
+                      }}
+                      isSelected={isYoutubeLinkSelected}
+                      tooltipLabels={{
+                        default: 'Enter Youtube Link',
+                        selected: 'Youtube Link entered!'
+                      }}
+                      variant={'transparent'}
+                      sx={(theme) => ({
+                        ...(Object.entries(theme.components.ActionIcon.styles(theme).root).find(
+                          (s) => s[0] === '&[data-variant="form"]'
+                        )[1] as object),
+                        '&[aria-selected="true"]': {
+                          color: theme.colors.red[5],
+                          backgroundColor: alpha(theme.colors.red[1], 0.5),
+
+                          '&:hover': {
+                            color: theme.colors.red[6],
+                            backgroundColor: theme.colors.red[1]
+                          }
+                        }
+                      })}
+                    />
+                  </Tooltip.Group>
+                </Group>
+
+                <Stack gap={0}>
+                  {!image && album?.imageUrl && (
+                    <Group gap={'xxs'}>
+                      <Center c={'primary.8'}>
+                        <IconInfoCircleFilled size={13} />
+                      </Center>
+
+                      <Text fw={500} c={'dimmed'} fz={'xs'}>
+                        The image will be inherited from the album.
+                      </Text>
+                    </Group>
+                  )}
+                  {!releaseDate && album?.releaseDate && (
+                    <Group gap={'xxs'}>
+                      <Center c={'primary.8'}>
+                        <IconInfoCircleFilled size={13} />
+                      </Center>
+
+                      <Text fw={500} c={'dimmed'} fz={'xs'}>
+                        The release date will be inherited from album.
+                      </Text>
+                    </Group>
+                  )}
+                </Stack>
+              </Stack>
             </Group>
 
             <Button style={{ alignSelf: 'center' }} type={'submit'} loading={isLoading}>
