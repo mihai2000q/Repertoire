993ae9593fa2485ce5a22d34f38abfca7625cabc
Cris <filipcristimihail@yahoo.com>
2025-05-21 14:32:25 +0200

    #502 UI - Hooks - use Topbar Height - return 0 when not logged in

--- repertoire.ui/src/hooks/useTopbarHeight.test.ts
+++ repertoire.ui/src/hooks/useTopbarHeight.test.ts
@@ -1,9 +1,19 @@
-import { renderHook } from '@testing-library/react'
 import useTopbarHeight from './useTopbarHeight.ts'
+import { reduxRenderHook } from '../test-utils.tsx'
 
 describe('use Topbar Height', () => {
-  it('should return 65 pixels', () => {
-    const { result } = renderHook(() => useTopbarHeight())
+  it('should return 65 pixels when logged in', () => {
+    const [{ result }] = reduxRenderHook(() => useTopbarHeight(), {
+      auth: {
+        token: 'something',
+        historyOnSignIn: undefined
+      }
+    })
     expect(result.current).toBe('65px')
+  })
+
+  it('should return 0 pixels when not logged in', () => {
+    const [{ result }] = reduxRenderHook(() => useTopbarHeight())
+    expect(result.current).toBe('0px')
   })
 })
