feaaabf4c338da0a0839ae6a702a97bf36483eb9
Cris <filipcristimihail@yahoo.com>
2025-05-22 15:15:37 +0200

    Revert "#502 UI - Home - Memoization when resizing"
    This reverts commit 33b25ffaf5b062c0058a03c90fd8c8556556fa15.

--- repertoire.ui/src/views/Home.tsx
+++ repertoire.ui/src/views/Home.tsx
@@ -1,4 +1,4 @@
-import { memo, ReactElement } from 'react'
+import { ReactElement } from 'react'
 import useFixedDocumentTitle from '../hooks/useFixedDocumentTitle.ts'
 import { SimpleGrid, Stack } from '@mantine/core'
 import HomeTop from '../components/home/HomeTop.tsx'
@@ -12,49 +12,38 @@ import useTopbarHeight from '../hooks/useTopbarHeight.ts'
 
 function Home(): ReactElement {
   useFixedDocumentTitle('Home')
+
+  const titleBarHeight = useTitleBarHeight()
+  const topbarHeight = useTopbarHeight()
   const stackGap = '16px'
   const { ref: topRef, height: topHeight } = useElementSize()
 
   return (
     <Stack h={'100%'} gap={stackGap}>
       <HomeTop ref={topRef} />
-      <HomeContent stackGap={stackGap} topHeight={topHeight} />
+
+      <SimpleGrid
+        px={'xl'}
+        cols={{ base: 1, md: 2, lg: 3 }}
+        h={`calc(100vh - ${topHeight}px - ${topbarHeight} - ${titleBarHeight} - ${stackGap})`}
+        pb={'lg'}
+        mih={300}
+      >
+        <HomeGenres visibleFrom={'md'} />
+        <HomeRecentlyPlayedSongs />
+        <Stack visibleFrom={'lg'}>
+          <HomeRecentArtists
+            sx={{
+              '@media(max-height: 850px)': {
+                display: 'none'
+              }
+            }}
+          />
+          <HomeRecentPlaylists flex={1} />
+        </Stack>
+      </SimpleGrid>
     </Stack>
   )
 }
-
-const HomeContent = memo(function HomeContent({
-  stackGap,
-  topHeight
-}: {
-  stackGap: string
-  topHeight: number
-}) {
-  const titleBarHeight = useTitleBarHeight()
-  const topbarHeight = useTopbarHeight()
-
-  return (
-    <SimpleGrid
-      px={'xl'}
-      cols={{ base: 1, md: 2, lg: 3 }}
-      h={`calc(100vh - ${topHeight}px - ${topbarHeight} - ${titleBarHeight} - ${stackGap})`}
-      pb={'lg'}
-      mih={300}
-    >
-      <HomeGenres visibleFrom={'md'} />
-      <HomeRecentlyPlayedSongs />
-      <Stack visibleFrom={'lg'}>
-        <HomeRecentArtists
-          sx={{
-            '@media(max-height: 850px)': {
-              display: 'none'
-            }
-          }}
-        />
-        <HomeRecentPlaylists flex={1} />
-      </Stack>
-    </SimpleGrid>
-  )
-})
 
 export default Home
