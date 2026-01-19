ab111ec91ac45608b2c142daff461f54505b3a17
Cris <filipcristimihail@yahoo.com>
2026-01-19 22:09:18 +0100

    #582 UI - Song - New Components: Song Sections Context Menu and Selection Drawer

---
+++ repertoire.ui/src/components/song/SongSectionsContextMenu.test.tsx
@@ -0,0 +1,163 @@
+import { reduxRender, withToastify } from '../../test-utils.tsx'
+import SongSectionsContextMenu from './SongSectionsContextMenu.tsx'
+import { screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { useDragSelect } from '../../context/DragSelectContext.tsx'
+import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw'
+import { BulkRehearsalsSongSectionsRequest } from '../../types/requests/SongRequests.ts'
+
+describe('Song Sections Context Menu', () => {
+  const dataTestId = 'dataTestId'
+  const selectedIds = ['1', '2', '3']
+  const clearSelection = vi.fn()
+
+  const handlers = [
+    http.get('/playlists', async () => {
+      return HttpResponse.json([])
+    })
+  ]
+
+  const server = setupServer(...handlers)
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+    vi.mocked(useDragSelect).mockReturnValue({
+      dragSelect: null,
+      selectedIds: selectedIds,
+      clearSelection: clearSelection
+    })
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+    server.resetHandlers()
+  })
+
+  beforeAll(() => {
+    server.listen()
+    // Mock the context
+    vi.mock('../../context/DragSelectContext', () => ({
+      useDragSelect: vi.fn()
+    }))
+  })
+
+  afterAll(() => server.close())
+
+  const render = (songId = '1') =>
+    reduxRender(
+      withToastify(
+        <SongSectionsContextMenu songId={songId}>
+          <div data-testid={dataTestId} />
+        </SongSectionsContextMenu>
+      )
+    )
+
+  it('should render', async () => {
+    const user = userEvent.setup()
+
+    render()
+
+    await user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByTestId(dataTestId)
+    })
+
+    expect(await screen.findByRole('menu')).toBeInTheDocument()
+    expect(screen.getByRole('menuitem', { name: /add rehearsals/i })).toBeInTheDocument()
+    expect(screen.getByRole('menuitem', { name: /delete/i })).toBeInTheDocument()
+  })
+
+  it('should be disabled when there are no selected ids', async () => {
+    const user = userEvent.setup()
+
+    vi.mocked(useDragSelect).mockReturnValue({
+      dragSelect: null,
+      selectedIds: [],
+      clearSelection: clearSelection
+    })
+
+    render()
+
+    await user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByTestId(dataTestId)
+    })
+
+    expect(screen.queryByRole('menu')).not.toBeInTheDocument()
+  })
+
+  it('should close menu when selected ids deplete', async () => {
+    const user = userEvent.setup()
+
+    // render and open menu
+    const [{ rerender }] = render()
+
+    await user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByTestId(dataTestId)
+    })
+    expect(screen.queryByRole('menu')).toBeInTheDocument()
+
+    // empty the selected ids and rerender the closed menu
+    vi.mocked(useDragSelect).mockReturnValue({
+      dragSelect: null,
+      selectedIds: [],
+      clearSelection: clearSelection
+    })
+
+    rerender(
+      <SongSectionsContextMenu songId={'1'}>
+        <div data-testid={dataTestId} />
+      </SongSectionsContextMenu>
+    )
+
+    expect(screen.queryByRole('menu')).not.toBeInTheDocument()
+  })
+
+  it('should bulk rehearsals by 1 on add rehearsals menu item', async () => {
+    const user = userEvent.setup()
+
+    let capturedRequest: BulkRehearsalsSongSectionsRequest
+    server.use(
+      http.post(`/songs/sections/bulk-rehearsals`, async (req) => {
+        capturedRequest = (await req.request.json()) as BulkRehearsalsSongSectionsRequest
+        return HttpResponse.json({ message: 'it worked' })
+      })
+    )
+
+    const songId = '1'
+
+    render(songId)
+
+    await user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByTestId(dataTestId)
+    })
+    await user.click(screen.getByRole('menuitem', { name: /add rehearsals/i }))
+    await user.click(screen.getByRole('button', { name: /confirm/i })) // menu item confirmation
+
+    expect(
+      screen.getByText(`Rehearsals added to ${selectedIds.length} sections!`)
+    ).toBeInTheDocument()
+    expect(capturedRequest).toStrictEqual({
+      sections: selectedIds.map((id) => ({ id: id, rehearsals: 1 })),
+      songId: songId
+    })
+    expect(clearSelection).toHaveBeenCalledOnce()
+  })
+
+  it('should open warning when clicking on delete menu item', async () => {
+    const user = userEvent.setup()
+
+    render()
+
+    await user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByTestId(dataTestId)
+    })
+    await user.click(screen.getByRole('menuitem', { name: /delete/i }))
+
+    expect(await screen.findByRole('dialog', { name: /delete sections/i })).toBeInTheDocument()
+  })
+})
---
+++ repertoire.ui/src/components/song/SongSectionsSelectionDrawer.tsx
@@ -0,0 +1,72 @@
+import { ActionIcon, Tooltip } from '@mantine/core'
+import { useDragSelect } from '../../context/DragSelectContext.tsx'
+import SelectionDrawer from '../@ui/drawer/SelectionDrawer.tsx'
+import { useDisclosure } from '@mantine/hooks'
+import { IconLocationPlus, IconTrash } from '@tabler/icons-react'
+import plural from '../../utils/plural.ts'
+import DeleteSongSectionsModal from './modal/DeleteSongSectionsModal.tsx'
+import { useBulkRehearsalsSongSectionsMutation } from '../../state/api/songsApi.ts'
+import { toast } from 'react-toastify'
+
+function SongSectionsSelectionDrawer({ songId }: { songId: string }) {
+  const { selectedIds, clearSelection } = useDragSelect()
+
+  const [openedDeleteWarning, { open: openDeleteWarning, close: closeDeleteWarning }] =
+    useDisclosure(false)
+
+  const [bulkRehearsals, { isLoading: bulkRehearsalsIsLoading }] =
+    useBulkRehearsalsSongSectionsMutation()
+
+  async function handleAddRehearsals() {
+    await bulkRehearsals({
+      sections: selectedIds.map((id) => ({ id: id, rehearsals: 1 })),
+      songId: songId
+    }).unwrap()
+    toast.success(`Rehearsals added to ${selectedIds.length} section${plural(selectedIds)}!`)
+    clearSelection()
+  }
+
+  return (
+    <>
+      <SelectionDrawer
+        aria-label={'song-sections-selection-drawer'}
+        opened={selectedIds.length > 0}
+        onClose={clearSelection}
+        text={`${selectedIds.length} section${plural(selectedIds)} selected`}
+        actionIcons={
+          <Tooltip.Group openDelay={200}>
+            <Tooltip label={'Add Rehearsals'} openDelay={200}>
+              <ActionIcon
+                aria-label={'add-rehearsals'}
+                variant={'grey-primary'}
+                loading={bulkRehearsalsIsLoading}
+                onClick={handleAddRehearsals}
+              >
+                <IconLocationPlus size={15} />
+              </ActionIcon>
+            </Tooltip>
+            <Tooltip label={'Delete song sections'}>
+              <ActionIcon
+                aria-label={'delete'}
+                variant={'grey-primary'}
+                onClick={openDeleteWarning}
+              >
+                <IconTrash size={18} />
+              </ActionIcon>
+            </Tooltip>
+          </Tooltip.Group>
+        }
+      />
+
+      <DeleteSongSectionsModal
+        ids={selectedIds}
+        songId={songId}
+        opened={openedDeleteWarning}
+        onClose={closeDeleteWarning}
+        onDelete={clearSelection}
+      />
+    </>
+  )
+}
+
+export default SongSectionsSelectionDrawer
