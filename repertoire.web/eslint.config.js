8f331b99df5d3ca66521975450dfa5611d137841
Cris <filipcristimihail@yahoo.com>
2026-01-22 12:55:51 +0100

    #596 Web - Update EsLint Config

--- repertoire.web/eslint.config.js
+++ repertoire.web/eslint.config.js
@@ -1,14 +1,28 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tslint from 'typescript-eslint'
+import reactLint from 'eslint-plugin-react'
+import prettier from 'eslint-config-prettier'
+import react from "@vitejs/plugin-react";
 
 export default tslint.config({
   files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   ignores: ['node_modules', 'dist', '.gitignore'],
-  extends: [js.configs.recommended, ...tslint.configs.recommended],
-  settings: { react: { version: 'detect' } },
+  extends: [
+    js.configs.recommended,
+    ...tslint.configs.recommended,
+    reactLint.configs.flat.recommended,
+    reactLint.configs.flat['jsx-runtime'],
+    prettier
+  ],
   languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser
-  }
+    ...reactLint.configs.flat.recommended.languageOptions,
+    ecmaVersion: 2022,
+    globals: {
+      ...globals.serviceworker,
+      ...globals.browser
+    }
+  },
+  plugins: { viteJs_react: react() },
+  settings: { react: { version: 'detect' } }
 })
