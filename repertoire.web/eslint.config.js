0fa50e95054a9dcfe0895aeb28ddef50d411d573
Cris <filipcristimihail@yahoo.com>
2026-01-22 12:36:30 +0100

    #596 Web - Update EsLint Config

--- repertoire.web/eslint.config.js
+++ repertoire.web/eslint.config.js
@@ -1,19 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tslint from "typescript-eslint";
+import js from '@eslint/js'
+import globals from 'globals'
+import tslint from 'typescript-eslint'
 
 export default tslint.config({
-  files: ["**/*.{ts,tsx}"],
-  ignores: ["node_modules", "dist", ".gitignore", "README.md"],
+  files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  ignores: ['node_modules', 'dist', '.gitignore'],
   extends: [js.configs.recommended, ...tslint.configs.recommended],
+  settings: { react: { version: 'detect' } },
   languageOptions: {
     ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-});
+    globals: globals.browser
+  }
+})
