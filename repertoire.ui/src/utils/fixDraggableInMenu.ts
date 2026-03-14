9b01a504b78e86d8807a31dbb3424d00aab8c449
Cris <filipcristimihail@yahoo.com>
2026-03-14 18:37:10 +0100

    #601 UI - Add Utils Function - fix Draggable In Menu

---
+++ repertoire.ui/src/utils/fixDraggableInMenu.ts
@@ -0,0 +1,12 @@
+export default function fixDraggableInMenu(draggableProps, snapshot: { isDragging: boolean }, offset = 160) {
+  if (snapshot.isDragging) {
+    if ('left' in draggableProps.style) {
+      draggableProps.style.left = 0
+    }
+    if ('top' in draggableProps.style) {
+      draggableProps.style.top -= offset
+    }
+  }
+
+  return draggableProps
+}
