6963baadd19df248ac821fec5a59c1696552fa37
Cris <filipcristimihail@yahoo.com>
2026-03-14 22:00:21 +0100

    #601 UI - Menu Components - Update Advanced Order Menu to use Menu Item

--- repertoire.ui/src/components/@ui/menu/AdvancedOrderMenu.tsx
+++ repertoire.ui/src/components/@ui/menu/AdvancedOrderMenu.tsx
@@ -7,7 +7,6 @@ import {
   Group,
   Menu,
   ScrollArea,
-  Space,
   Stack,
   Text,
   Tooltip
@@ -15,6 +14,7 @@ import {
 import { MouseEvent, ReactElement, ReactNode, useState } from 'react'
 import OrderType from '../../../types/enums/OrderType.ts'
 import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
+import fixDraggableInMenu from '../../../utils/fixDraggableInMenu.ts'
 
 interface AdvancedOrderMenuProps {
   children: ReactNode
@@ -67,62 +67,56 @@ function AdvancedOrderMenu({ children, orders, setOrders, propertyIcons }: Advan
           <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="dnd-list" direction="vertical">
               {(provided) => (
-                <Stack gap={2} ref={provided.innerRef} {...provided.droppableProps}>
+                <Stack ref={provided.innerRef} {...provided.droppableProps} gap={2}>
                   {orders.map((order, index) => (
                     <Draggable key={order.label} index={index} draggableId={order.label}>
-                      {(provided, snapshot) => {
-                        if (snapshot.isDragging) {
-                          if ('left' in provided.draggableProps.style) {
-                            provided.draggableProps.style.left = 0
-                          }
-                          if ('top' in provided.draggableProps.style) {
-                            provided.draggableProps.style.top -= 160
-                          }
-                        }
-
-                        return (
-                          <Group
-                            role={'button'}
-                            aria-label={order.label}
-                            ref={provided.innerRef}
-                            data-active={order.checked ?? false}
-                            gap={0}
-                            sx={(theme) => ({
-                              transition: '0.2s',
-                              borderRadius: theme.radius.md,
+                      {(provided, snapshot) => (
+                        <Menu.Item
+                          component={'div'}
+                          ref={provided.innerRef}
+                          aria-label={order.label}
+                          data-active={order.checked ?? false}
+                          px={12}
+                          py={8}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          style={(theme) => ({
+                            ...fixDraggableInMenu(provided.draggableProps, snapshot, 79).style,
+                            ...(snapshot.isDragging && {
+                              cursor: 'grabbing',
+                              borderColor: theme.colors.primary[3]
+                            })
+                          })}
+                          sx={(theme) => ({
+                            transition: '0.2s',
+                            cursor:
+                              order.checked === true && isOnlyOneOrderActive
+                                ? 'default'
+                                : 'pointer',
+                            border: '1px solid transparent',
+                            color: theme.colors.gray[6],
+                            backgroundColor: alpha(theme.white, 0.7),
+                            '&:hover': {
                               color: theme.colors.gray[6],
-                              '&:hover': {
-                                color: theme.colors.gray[6],
-                                backgroundColor: theme.colors.gray[1]
-                              },
+                              backgroundColor: theme.colors.gray[1]
+                            },
 
-                              ...(order.checked && {
-                                color: theme.colors.primary[4],
-                                backgroundColor: alpha(theme.colors.primary[1], 0.4),
-                                '&:hover': {
-                                  color: isOnlyOneOrderActive
-                                    ? theme.colors.primary[4]
-                                    : theme.colors.primary[5],
-                                  backgroundColor: isOnlyOneOrderActive
-                                    ? alpha(theme.colors.primary[1], 0.4)
-                                    : alpha(theme.colors.primary[1], 0.8)
-                                }
-                              })
-                            })}
-                            px={12}
-                            py={8}
-                            {...provided.dragHandleProps}
-                            {...provided.draggableProps}
-                            style={{
-                              ...provided.draggableProps.style,
-                              cursor: snapshot.isDragging
-                                ? 'grabbing'
-                                : order.checked === true && isOnlyOneOrderActive
-                                  ? 'default'
-                                  : 'pointer'
-                            }}
-                            onClick={() => onClick(order, index)}
-                          >
+                            ...(order.checked && {
+                              color: theme.colors.primary[4],
+                              backgroundColor: alpha(theme.colors.primary[1], 0.4),
+                              '&:hover': {
+                                color: isOnlyOneOrderActive
+                                  ? theme.colors.primary[4]
+                                  : theme.colors.primary[5],
+                                backgroundColor: isOnlyOneOrderActive
+                                  ? alpha(theme.colors.primary[1], 0.4)
+                                  : alpha(theme.colors.primary[1], 0.8)
+                              }
+                            })
+                          })}
+                          onClick={() => onClick(order, index)}
+                        >
+                          <Group gap={0}>
                             <Center w={15} h={15}>
                               {propertyIcons?.get(order.property)}
                             </Center>
@@ -132,10 +126,10 @@ function AdvancedOrderMenu({ children, orders, setOrders, propertyIcons }: Advan
                               inline
                               pl={8}
                               pr={4}
+                              flex={1}
                             >
                               {order.label}
                             </Text>
-                            <Space flex={1} />
                             {order.checked === true && (
                               <Tooltip label={'Change Order'} openDelay={200}>
                                 <ActionIcon
@@ -157,8 +151,8 @@ function AdvancedOrderMenu({ children, orders, setOrders, propertyIcons }: Advan
                               </Tooltip>
                             )}
                           </Group>
-                        )
-                      }}
+                        </Menu.Item>
+                      )}
                     </Draggable>
                   ))}
                   {provided.placeholder}
