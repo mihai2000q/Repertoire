d5df47ab5fc61e1d6634eb49867bd931780b80fb
Cris <filipcristimihail@yahoo.com>
2026-06-05 17:52:38 +0200

    #596 UI - Sign Up - fix form validation

--- repertoire.ui/src/validation/signUpForm.ts
+++ repertoire.ui/src/validation/signUpForm.ts
@@ -4,11 +4,11 @@ export const signUpSchema = z.object({
   email: z.email('Email is invalid'),
   password: z
     .string()
+    .refine((val) => val.length > 0, 'Password cannot be blank')
+    .refine((val) => val.length >= 8, 'Password must have at least 8 characters')
     .refine((val) => /[A-Z]/.test(val), 'Password must have at least 1 upper character')
     .refine((val) => /[a-z]/.test(val), 'Password must have at least 1 lower character')
-    .refine((val) => /[0-9]/.test(val), 'Password must have at least 1 digit')
-    .refine((val) => val.length >= 8, 'Password must have at least 8 characters')
-    .refine((val) => val.length > 0, 'Password cannot be blank'),
+    .refine((val) => /[0-9]/.test(val), 'Password must have at least 1 digit'),
   name: z.string().trim().min(1, 'Name cannot be blank')
 })
 export type SignUpForm = z.infer<typeof signUpSchema>
