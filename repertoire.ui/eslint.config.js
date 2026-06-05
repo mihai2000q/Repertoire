ebf252e8c998e17d8ad981ee1b8839da7ae03728
Cris <filipcristimihail@yahoo.com>
2026-06-05 12:49:42 +0200

    #596 UI - Add React Hooks to EsLint

--- repertoire.ui/eslint.config.js
+++ repertoire.ui/eslint.config.js
@@ -2,31 +2,38 @@ import globals from 'globals'
 import jsLint from '@eslint/js'
 import tsLint from 'typescript-eslint'
 import reactLint from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
 import prettier from 'eslint-config-prettier'
 
-export default tsLint.config({
-  files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-  extends: [
-    jsLint.configs.recommended,
-    ...tsLint.configs.recommended,
-    reactLint.configs.flat.recommended,
-    reactLint.configs.flat['jsx-runtime'],
-    prettier
-  ],
-  ignores: ['node_modules', '.gitignore'],
-  languageOptions: {
-    ...reactLint.configs.flat.recommended.languageOptions,
-    globals: {
-      ...globals.serviceworker,
-      ...globals.browser,
-      ...globals.node
+export default tsLint.config(
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['node_modules', '.gitignore'],
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    settings: { react: { version: 'detect' } }
+  },
+  jsLint.configs.recommended,
+  ...tsLint.configs.recommended,
+  reactLint.configs.flat.recommended,
+  reactLint.configs.flat['jsx-runtime'],
+  reactHooks.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-console': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/refs': 'off'
     }
   },
-  settings: { react: { version: 'detect' } },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'no-console': 'warn',
-    '@typescript-eslint/no-require-imports': 'warn'
-  }
-})
+  prettier
+)
