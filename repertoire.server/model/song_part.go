064347962d34695a375b750a5f99dd250d0f87e1
Cris <filipcristimihail@yahoo.com>
2026-08-29 17:35:36 +0200

    #617 Server - Extract parts of Song Section into New Model: Song Part and Update Song Arrangement to be used in concordance with the parts instead of the sections

---
+++ repertoire.server/model/song_part.go
@@ -0,0 +1,54 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SongPart struct {
+	ID           uuid.UUID `gorm:"primaryKey; type:uuid; <-:create" json:"id"`
+	Name         string    `gorm:"size:30" json:"name"`
+	SongOrder    uint      `gorm:"not null" json:"-"`
+	SectionOrder *uint     `json:"-"`
+
+	Rehearsals      uint   `gorm:"not null" json:"rehearsals"`
+	Confidence      uint   `gorm:"not null" json:"confidence"`
+	RehearsalsScore uint64 `gorm:"not null" json:"rehearsalsScore"`
+	ConfidenceScore uint   `gorm:"not null" json:"confidenceScore"`
+	Progress        uint64 `gorm:"not null" json:"progress"`
+
+	SongID       uuid.UUID  `gorm:"not null; index: idx_song_instrument_parts_song_id" json:"-"`
+	SectionID    *uuid.UUID `gorm:"not null; index: idx_song_instrument_parts_song_section_id" json:"-"`
+	BandMemberID *uuid.UUID `json:"-"`
+	InstrumentID *uuid.UUID `json:"-"`
+
+	Section    SongSection `json:"-"`
+	BandMember *BandMember `json:"bandMember"`
+	Instrument *Instrument `json:"instrument"`
+
+	History                []SongPartHistory     `gorm:"constraint:OnDelete:CASCADE" json:"-"`
+	ArrangementOccurrences []SongPartOccurrences `gorm:"foreignKey:PartID; constraint:OnDelete:CASCADE" json:"-"`
+
+	CreatedAt time.Time `gorm:"default:current_timestamp; not null; <-:create" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"default:current_timestamp; not null" json:"updatedAt"`
+}
+
+type SongPartHistory struct {
+	ID         uuid.UUID        `gorm:"primaryKey; type:uuid; <-:create"`
+	Property   SongPartProperty `gorm:"size:255; not null"`
+	From       uint             `gorm:"not null"`
+	To         uint             `gorm:"not null"`
+	SongPartID uuid.UUID        `gorm:"not null; index:idx_song_instrument_part_histories_song_instrument_part_id"`
+
+	CreatedAt time.Time `gorm:"default:current_timestamp; not null; <-:create"`
+}
+
+type SongPartProperty string
+
+const (
+	ConfidenceProperty SongPartProperty = "Confidence"
+	RehearsalsProperty SongPartProperty = "Rehearsals"
+)
+
+var DefaultSongSectionConfidence uint = 0
--- repertoire.server/model/song_section.go
+++ repertoire.server/model/song_section.go
@@ -11,44 +11,18 @@ type SongSection struct {
 	Name  string    `gorm:"size:30" json:"name"`
 	Order uint      `gorm:"not null" json:"-"`
 
-	Rehearsals      uint   `gorm:"not null" json:"rehearsals"`
-	Confidence      uint   `gorm:"not null; size:100" json:"confidence"`
-	RehearsalsScore uint64 `gorm:"not null" json:"rehearsalsScore"`
-	ConfidenceScore uint   `gorm:"not null" json:"confidenceScore"`
-	Progress        uint64 `gorm:"not null" json:"progress"`
+	Rehearsals float64 `gorm:"not null" json:"rehearsals"`
+	Confidence float64 `gorm:"not null" json:"confidence"`
+	Progress   float64 `gorm:"not null" json:"progress"`
 
-	SongID            uuid.UUID  `gorm:"not null; index: idx_song_sections_song_id" json:"-"`
-	SongSectionTypeID uuid.UUID  `gorm:"not null" json:"-"`
-	InstrumentID      *uuid.UUID `json:"-"`
-	BandMemberID      *uuid.UUID `json:"-"`
+	SongID            uuid.UUID `gorm:"not null; index: idx_song_sections_song_id" json:"-"`
+	SongSectionTypeID uuid.UUID `gorm:"not null" json:"-"`
 
 	Song            Song            `json:"-"`
 	SongSectionType SongSectionType `json:"songSectionType"`
-	BandMember      *BandMember     `json:"bandMember"`
-	Instrument      *Instrument     `json:"instrument"`
 
-	History                []SongSectionHistory     `gorm:"constraint:OnDelete:CASCADE" json:"-"`
-	ArrangementOccurrences []SongSectionOccurrences `gorm:"foreignKey:SectionID; constraint:OnDelete:CASCADE" json:"-"`
+	Parts []SongPart `gorm:"foreignKey:SectionID; constraint:OnDelete:CASCADE" json:"-"`
 
 	CreatedAt time.Time `gorm:"default:current_timestamp; not null; <-:create" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"default:current_timestamp; not null" json:"updatedAt"`
 }
-
-type SongSectionHistory struct {
-	ID            uuid.UUID           `gorm:"primaryKey; type:uuid; <-:create"`
-	Property      SongSectionProperty `gorm:"size:255; not null"`
-	From          uint                `gorm:"not null"`
-	To            uint                `gorm:"not null"`
-	SongSectionID uuid.UUID           `gorm:"not null; index:idx_song_section_histories_section_id"`
-
-	CreatedAt time.Time `gorm:"default:current_timestamp; not null; <-:create"`
-}
-
-type SongSectionProperty string
-
-const (
-	ConfidenceProperty SongSectionProperty = "Confidence"
-	RehearsalsProperty SongSectionProperty = "Rehearsals"
-)
-
-var DefaultSongSectionConfidence uint = 0
