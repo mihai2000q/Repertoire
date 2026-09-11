03465a2dc9e62332fc16b59c6c99585ddbfb561b
Cris <filipcristimihail@yahoo.com>
2026-09-12 00:42:08 +0200

    #617 UI - Song - Outline - New Component: View Control

---
+++ repertoire.ui/src/features/songs/features/song/features/outline/components/toolbar/SongOutlineViewControl.tsx
@@ -0,0 +1,60 @@
+import { Center, SegmentedControl, Tooltip } from '@mantine/core'
+import { IconList, IconListTree } from '@tabler/icons-react'
+import useLocalStorage from '../../../../../../../../hooks/useLocalStorage.ts'
+import LocalStorageKeys from '../../../../../../../../types/enums/keys/LocalStorageKeys.ts'
+import { useEffect } from 'react'
+import { useAppDispatch } from '../../../../../../../../state/store.ts'
+import OutlineView from '../../types/enums/OutlineView.ts'
+import { setView } from '../../state/slice/songOutlineSlice.ts'
+
+function SongOutlineViewControl() {
+  const dispatch = useAppDispatch()
+
+  const [outlineView, localSetOutlineView] = useLocalStorage({
+    key: LocalStorageKeys.SongOutlineView,
+    defaultValue: OutlineView.Sections
+  })
+  useEffect(() => {
+    dispatch(setView(outlineView))
+  }, [outlineView])
+
+  return (
+    <SegmentedControl<OutlineView>
+      value={outlineView}
+      onChange={localSetOutlineView}
+      size={'xs'}
+      color={'gray'}
+      radius={'12px'}
+      data={[
+        {
+          value: OutlineView.Sections,
+          label: (
+            <Tooltip label={'Sections View'}>
+              <Center>
+                <IconListTree size={16} aria-label={'sections-view'} />
+              </Center>
+            </Tooltip>
+          )
+        },
+        {
+          value: OutlineView.Parts,
+          label: (
+            <Tooltip label={'Parts View'}>
+              <Center>
+                <IconList size={16} aria-label={'parts-view'} />
+              </Center>
+            </Tooltip>
+          )
+        }
+      ]}
+      styles={{
+        label: {
+          width: '24px',
+          padding: '3px'
+        }
+      }}
+    />
+  )
+}
+
+export default SongOutlineViewControl
