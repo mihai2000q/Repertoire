485d1f6dc75592e680b4b50de75d884dd1592a7b
Cris <filipcristimihail@yahoo.com>
2026-09-11 19:18:43 +0200

    #617 UI - Song - Sections - New Components: Band Member and Instruments Group; and Song Section Type Badge

---
+++ repertoire.ui/src/features/songs/features/song/features/sections/components/section/SongSectionTypeBadge.tsx
@@ -0,0 +1,26 @@
+import { SongSectionType } from '../../../../../../../../types/models/Song.ts'
+import { Text } from '@mantine/core'
+
+interface SongSectionTypeBadgeProps {
+  songSectionType: SongSectionType
+}
+
+function SongSectionTypeBadge({ songSectionType }: SongSectionTypeBadgeProps) {
+  return (
+    <Text
+      fw={600}
+      fz={'xs'}
+      c={'gray.5'}
+      bg={'gray.1'}
+      px={'5px'}
+      py={'2px'}
+      style={(theme) => ({
+        borderRadius: theme.radius.md
+      })}
+    >
+      {songSectionType.name}
+    </Text>
+  )
+}
+
+export default SongSectionTypeBadge
