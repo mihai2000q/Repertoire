b7b933ec62853fb3d56cc040ceb300dd32bf9e06
Cris <filipcristimihail@yahoo.com>
2026-09-20 12:59:55 +0200

    #617 UI - Refactor Features/Artists/Features/Artist to Features/Artist

--- repertoire.ui/src/features/artists/features/artist/components/loader/ArtistAlbumsLoader.test.tsx
+++
@@ -1,8 +0,0 @@
-import { mantineRender } from '../../../../../../test-utils.tsx'
-import ArtistAlbumsLoader from './ArtistAlbumsLoader.tsx'
-
-describe('Artist Albums Loader', () => {
-  it('should render', () => {
-    mantineRender(<ArtistAlbumsLoader />)
-  })
-})
--- repertoire.ui/src/features/artists/features/artist/components/loader/ArtistSongsLoader.test.tsx
+++
@@ -1,8 +0,0 @@
-import { mantineRender } from '../../../../../../test-utils.tsx'
-import ArtistSongsLoader from './ArtistSongsLoader.tsx'
-
-describe('Artist Songs Loader', () => {
-  it('should render', () => {
-    mantineRender(<ArtistSongsLoader />)
-  })
-})
