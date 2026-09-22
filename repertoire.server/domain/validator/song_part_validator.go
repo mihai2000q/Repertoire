a0b160a65e1afe7d72347a562df087efa97b7cae
Cris <filipcristimihail@yahoo.com>
2026-09-22 16:49:15 +0200

    #617 Server - Domain - New Validator: Song Part Validator

--- repertoire.server/domain/validator/song_part_validator.go
+++ repertoire.server/domain/validator/song_part_validator.go
@@ -1,1 +1,46 @@
 package validator
+
+import (
+	"errors"
+	"repertoire/server/data/repository"
+	"repertoire/server/internal/httperror"
+	"repertoire/server/model"
+
+	"github.com/google/uuid"
+)
+
+type SongPartValidator interface {
+	Validate(ids []uuid.UUID, songID uuid.UUID) *httperror.ErrorCode
+}
+
+type songPartValidator struct {
+	songPartRepository repository.SongPartRepository
+}
+
+func NewSongPartValidator(songPartRepository repository.SongPartRepository) SongPartValidator {
+	return songPartValidator{
+		songPartRepository: songPartRepository,
+	}
+}
+
+func (s songPartValidator) Validate(ids []uuid.UUID, songID uuid.UUID) *httperror.ErrorCode {
+	if len(ids) == 0 {
+		return nil
+	}
+
+	var parts []model.SongPart
+	if err := s.songPartRepository.GetAllByIDs(&parts, ids); err != nil {
+		return httperror.DatabaseError(err)
+	}
+	if len(parts) != len(ids) {
+		return httperror.NotFoundError(errors.New("parts not found"))
+	}
+
+	for _, part := range parts {
+		if part.SongID != songID {
+			return httperror.ConflictError(errors.New("song part does not belong to the same song as the section"))
+		}
+	}
+
+	return nil
+}
--- repertoire.server/test/unit/domain/validator/song_part_validator_mock.go
+++ repertoire.server/test/unit/domain/validator/song_part_validator_mock.go
@@ -1,1 +1,23 @@
 package validator
+
+import (
+	"repertoire/server/internal/httperror"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+)
+
+type SongPartValidatorMock struct {
+	mock.Mock
+}
+
+func (s *SongPartValidatorMock) Validate(ids []uuid.UUID, songID uuid.UUID) *httperror.ErrorCode {
+	args := s.Called(ids, songID)
+
+	var errCode *httperror.ErrorCode
+	if args.Get(0) != nil {
+		errCode = args.Get(0).(*httperror.ErrorCode)
+	}
+
+	return errCode
+}
